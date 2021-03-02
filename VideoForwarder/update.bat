2457e68f840e4e510585725c4c44b928bdb2790d
J.Teeuwissen <j.teeuwissen@students.uu.nl>
2021-03-02 23:02:25 +0100

    UFT: add pulling support using ffmpeg We now have a place to define pulling options to fill the forwarding server with data.

---
+++ VideoForwarder/update.bat
@@ -0,0 +1,14 @@
+set imageName=videoforwarderimage
+set containerName=videoforwardercontainer
+
+:: Remove old container
+docker rm -f %containerName%
+
+:: Remove old image
+docker image rm %imageName%
+
+:: Create new image
+docker build -t %imageName% .
+
+:: Run new container, this one exposes port 80 and 1935
+docker run -d -p 80:8080 -p 1935:1935  --name %containerName% %imageName%
