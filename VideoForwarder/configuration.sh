19e8ae45c5377a552b4a81c59ed3c7e998a79c1b
J.Teeuwissen <j.teeuwissen@students.uu.nl>
2021-03-06 20:52:26 +0100

    UFT: limited bitrate amount and dynamic conf added a dynamic conf instead of the static conf to allow for parameterized cameras. changes the bitrates to have the same resolution with a lower quallity (not sure if works).

---
+++ VideoForwarder/configuration.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+# config +urls
+
+CONFIG="/usr/local/nginx/conf/nginx.conf"
+> $CONFIG
+
+echo "events {
+
+}
+
+
+# RTMP configuration
+rtmp {
+  server {
+    # Print to log so we can see what happens
+    access_log /dev/stdout;
+
+    # Listen for incoming stream on standard RTMP port
+    listen 1935;
+
+    # Set the chunk size
+    chunk_size 4000;
+
+    application pull {
+      # Live on
+      live on;
+
+      # Run this from start" >> $CONFIG
+
+for i in $@
+do
+    ARR=(${i//;/ })
+    echo "      exec_static /usr/bin/ffmpeg -i ${ARR[0]} -vcodec libx264 -c:a aac -f flv rtmp://localhost/live/${ARR[1]};" >> $CONFIG
+done
+
+echo "
+     # disable consuming the stream from nginx as rtmp
+      deny play all;
+    }
+
+    application live {
+      live on;
+
+      exec /usr/bin/ffmpeg -i rtmp://localhost/\$app/\$name -async 1 -vsync -1
+      -c:v libx264 -c:a libvo_aacenc -b:v 128K -b:a 32k -vf \"scale=1280:720\" -tune zerolatency -preset veryfast -crf 23 -f flv rtmp://localhost/show/\$name_low
+      -c:v libx264 -c:a libvo_aacenc -b:v 256k -b:a 64k -vf \"scale=1280:720\" -tune zerolatency -preset veryfast -crf 23 -f flv rtmp://localhost/show/\$name_mid
+      -c:v libx264 -c:a libvo_aacenc -b:v 512K -b:a 128k -vf \"scale=1280:720\" -tune zerolatency -preset veryfast -crf 23 -f flv rtmp://localhost/show/\$name_high;
+    }
+
+    application show {
+      live on;
+      hls on;
+
+      # Where to stream the data to locally
+      hls_path /mnt/hls/;
+
+      # Amount of seconds per fragment
+      hls_fragment 1;
+
+      # Amount of seconds total for playlist
+      hls_playlist_length 30;
+
+      # Instruct clients to adjust resolution according to bandwidth
+      hls_variant _low BANDWIDTH=160000; # Low bitrate, sub-SD resolution
+      hls_variant _mid BANDWIDTH=320000; # Medium bitrate, SD resolution
+      hls_variant _high BANDWIDTH=640000; # High bitrate, higher-than-SD resolution
+    }
+  }
+}
+
+http {
+  sendfile off;
+  tcp_nopush on;
+
+  directio 512;
+  default_type application/octet-stream;
+
+  # Print to log so we can see what happens
+  access_log /dev/stdout;
+  error_log /dev/stdout;
+
+  server {
+    listen 8080;
+
+    location / {
+      # Disable cache
+      add_header 'Cache-Control' 'no-cache';
+
+      # CORS setup
+      add_header 'Access-Control-Allow-Origin' '*' always;
+      add_header 'Access-Control-Expose-Headers' 'Content-Length';
+
+      # allow CORS preflight requests
+      if (\$request_method = 'OPTIONS') {
+        add_header 'Access-Control-Allow-Origin' '*';
+        add_header 'Access-Control-Max-Age' 1728000;
+        add_header 'Content-Type' 'text/plain charset=UTF-8';
+        add_header 'Content-Length' 0;
+        return 204;
+      }
+
+      types {
+        application/vnd.apple.mpegurl m3u8;
+        video/mp2t ts;
+      }
+
+      root /mnt/;
+    }
+  }
+}" >> $CONFIG
+
+/usr/local/nginx/sbin/nginx -g "daemon off;"
