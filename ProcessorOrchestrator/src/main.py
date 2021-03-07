2729834fa7043e8b92b06b97f803258fdda00aab
Rienk <rienkfidder@gmail.com>
2021-03-07 11:08:21 +0100

    FFT: Implemented first client websocket Added file to handle client request and add simple client websocket that enlists itself to a list of sockets and echoes any message it gets to all sockets.

--- ProcessorOrchestrator/src/main.py
+++ ProcessorOrchestrator/src/main.py
@@ -3,22 +3,27 @@ from tornado.ioloop import IOLoop
 from tornado.options import define, options
 from tornado.web import Application
 from tornado.web import RequestHandler
+from clientConnection import ClientSocket
 
 define('port', default=8000, help='port to listen on')
+
 
 def main():
     """Construct and serve the tornado application."""
     app = Application([
-        ('/', HelloWorld)
+        ('/', HelloWorld),
+        ('/client', ClientSocket)
     ])
     http_server = HTTPServer(app)
-    http_server.listen(8000)
+    http_server.listen(options.port)
     print('Listening on http://localhost:%i' % options.port)
     IOLoop.current().start()
+
 
 class HelloWorld(RequestHandler):
     def get(self):
         self.write("Hello World")
+
 
 if __name__ == "__main__":
     main()
