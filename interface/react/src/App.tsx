b5e461fb639073fa966041113f5cc07abe8b80ad
J.Teeuwissen <j.teeuwissen@students.uu.nl>
2021-03-09 20:41:25 +0100

    FFT: seperate react folder & config to load streams loading the streams from a public file (should get secured tho) and updated the registry file for it.

---
+++ interface/react/src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { render } from 'react-dom';
+import VideoPlayer from "./components/VideojsPlayer";
+import logo from './logo.svg';
+import './App.css';
+import { Stream } from 'node:stream';
+
+type stream = { name: string, url: string }
+type appState = { streams: stream[] }
+
+class App extends React.Component<{}, appState> {
+
+  constructor(props) {
+    super(props);
+    this.state = { streams: [] }
+  }
+
+  async componentDidMount() {
+    var config = await (await fetch(process.env.PUBLIC_URL + '/config.json')).json();
+    this.setState({ streams: config.map((stream) => ({ name: stream.Name, url: stream.Forwarder })) })
+  }
+
+  render() {
+    var sources = this.state.streams.map((stream) => ({
+      name: stream.name,
+      srcObject: {
+        src: stream.url,
+        type: 'application/x-mpegURL'
+      }
+    }))
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            sources && sources.map((source) =>
+              <div>
+                <h1>{source.name}</h1>
+                <VideoPlayer key={source.name} autoplay={true} controls={true} sources={[source.srcObject]} />
+              </div>)
+          }
+        </header>
+      </div>
+    );
+  }
+}
+
+console.log("Test: " + process.env.NODE_ENV)
+console.log("Environment variable: " + process.env.REACT_APP_URL) //alle var moeten met REACT_APP_ beginnen
+
+export default App;
--- interface/react/src/components/VideojsPlayer.tsx
+++ interface/react/src/components/VideojsPlayer.tsx
@@ -4,45 +4,34 @@ import videojs from 'video.js';
 // Styles
 import 'video.js/dist/video-js.css';
 
-interface VideoPlayerPropsInferface {
-    videoJsOptions: videojs.PlayerOptions;
-}
-
-class VideoPlayer extends React.Component {
+class VideoPlayer extends React.Component<videojs.PlayerOptions> {
     private player?: videojs.Player;
     private videoNode?: HTMLVideoElement;
 
-    constructor(props: VideoPlayerPropsInferface) {
-        super(props);
-        this.player = undefined;
-        this.videoNode = undefined;
-    }
-
     componentDidMount() {
-
         // instantiate video.js
-        this.player = videojs(this.videoNode, this.props).ready(function() {
-            // console.log('onPlayerReady', this);
-        });
+        this.player = videojs(this.videoNode, this.props).ready(function () {
+            //console.log('onPlayerReady')
+        })
     }
+
 
     // destroy player on unmount
     componentWillUnmount() {
-        if (this.player) {
-            this.player.dispose();
-        }
+        this.player?.dispose()
     }
 
     // wrap the player in a div with a `data-vjs-player` attribute
     // so videojs won't create additional wrapper in the DOM
     // see https://github.com/videojs/video.js/pull/3856
     render() {
+        console.log("render")
         return (
             <div className="c-player">
                 <div className="c-player__screen" data-vjs-player="true">
                     <video ref={(node: HTMLVideoElement) => this.videoNode = node} className="video-js" />
                 </div>
-{}                {/*<div className="c-player__controls">
+                {}                {/*<div className="c-player__controls">
                     <button>Play</button>
                     <button>Pause</button>
                 </div>*/}
