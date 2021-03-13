0f64d1ac9a36e8556b631c19a02fa825171846a0
Tim <timvankemenade@outlook.com>
2021-03-13 17:42:19 +0100

    DOC: documented execution of example_plan.py

--- CameraProcessor/scheduling/plan/example_plan.py
+++ CameraProcessor/scheduling/plan/example_plan.py
@@ -21,10 +21,15 @@ schedule_input_node = ScheduleNode(1, [(layer1_node1, 0), (layer1_node2, 0)], In
 if __name__ == '__main__':
     from scheduling.scheduler import Scheduler
 
+    # Inits scheduler with starting node.
     scheduler = Scheduler(schedule_input_node)
+    # Runs iteration of schedule_graph.
     scheduler.schedule_graph("test")
 
+    # Find output in output node.
     curr_node = schedule_input_node
     while len(curr_node.out_nodes) > 0:
         curr_node = curr_node.out_nodes[0][0]
+
+    # Print output of output node.
     print(curr_node.component.out)
