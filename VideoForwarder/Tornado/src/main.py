ce118d4ca83ce42a18bf5fffd8fb997b2471cf63
J.Teeuwissen <j.teeuwissen@students.uu.nl>
2021-03-17 15:18:14 +0100

    UFT: added place to insert ffmpeg logic

--- VideoForwarder/Tornado/src/main.py
+++ VideoForwarder/Tornado/src/main.py
@@ -2,9 +2,17 @@ import os
 import tornado.web
 import tornado.ioloop
 
+class fileHandler(tornado.web.StaticFileHandler):
+    def get_absolute_path(self, root, path):
+        abspath = os.path.abspath(os.path.join(root, path))
+        if not os.path.exists(abspath):
+            print("not found")
+            #Create the file using ffmpeg
+        return abspath
+
 if __name__ == "__main__":
-    app = tornado.web.Application([
-        (r'/(.*)', tornado.web.StaticFileHandler, {'path': '/streams/'})
+    app = tornado.web.Application([ 
+        (r"/(.*)", fileHandler, {'path': '/streams/'})
     ])
 
     app.listen(80)
