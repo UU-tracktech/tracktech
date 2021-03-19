0d272e26ddf8e6ab18643468317addd380cd9b20
Kwa,J.T.S. (Jaimy) <j.t.s.kwa@students.uu.nl>
2021-03-19 16:56:53 +0100

    UFT Read stream frame by frame and put it back inside video container UFT DetectionObj can now draw rectangles on frame
    Former-commit-id: 9e4cfaeda436e7e0ffb712134c7ae1383a3a3fd0

--- CameraProcessor/detection/bounding_box.py
+++ CameraProcessor/detection/bounding_box.py
@@ -1,7 +1,7 @@
 class BoundingBox:
     def __init__(self, identifier, rectangle, classification, certainty):
         self.identifier = identifier
-        self.rectangle = rectangle
+        self.rectangle = rectangle  #[x1, y1, x2, y2] where (x1, y1) and (x2, y2) are two diagonal opposing corners of the box
         self.feature = None
         self.classification = classification
         self.certainty = certainty
