3e6698b41cc5996884a747ee313af20c5618355c
saberzero1 <ewjbangma@hotmail.com>
2021-03-21 15:05:38 +0100

    BED Updated video.js to Version 7 from 6

--- interface/react/Dependencies.bat
+++ interface/react/Dependencies.bat
@@ -1,4 +1,4 @@
-call npm install videojs
+call npm install video.js@7.11.4
 call npm install -f jest@26.6.0
 call npm install -f jest babel-jest @babel/preset-env
 call npm install -f --save chokidar
