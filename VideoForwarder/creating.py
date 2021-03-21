6e0be4383dab56c38286e6f678b7de2b5ef9ac6b
Li-Yeun <45470843+Li-Yeun@users.noreply.github.com>
2021-03-21 17:56:15 +0100

    OTH: removed test file

--- VideoForwarder/creating.py
+++
@@ -1,6 +0,0 @@
-import os
-
-base = 'C:/Users/liyeu/Desktop/KI/master'
-f = open(base + '.txt', "w+")
-f.write(f'#EXTM3U\r\n#EXT-X-VERSION:3\r\n#EXT-X-STREAM-INF:BANDWIDTH=800000,RESOLUTION=640x360\r\ncam1_360p.m3u8\r\n#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=842x480\r\ncam1_480p.m3u8\r\n#EXT-X-STREAM-INF:BANDWIDTH=2800000,RESOLUTION=1280x720\r\ncam1_720p.m3u8\r\n#EXT-X-STREAM-INF:BANDWIDTH=5000000,RESOLUTION=1920x1080\r\ncam1_1080p.m3u8')
-os.rename(base + '.txt', 'C:/Users/liyeu/Desktop/KI/master.m3u8')
