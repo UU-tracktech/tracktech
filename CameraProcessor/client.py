67f1beba8f4331cc7332aca0cf2119ab05f20375
Max van der Hart <m.vanderhart@students.uu.nl>
2021-03-22 11:01:19 +0100

    UFT: Cleaning up, non blocking write

--- CameraProcessor/client.py
+++ CameraProcessor/client.py
@@ -1,142 +1,3 @@
-
-# from tornado import escape
-# from tornado import gen
-# from tornado import ioloop
-# from tornado import websocket
-# import json
-# import time
-#
-#
-#
-#
-# class WebSocketClient():
-#     """Class that can communicate with websockets
-#     """
-#
-#     def __init__(self, url):
-#         """:param str url: URL of websocket to connect to"""
-#         self._url = url
-#
-#     def connect(self):
-#         """Connect to the websocket"""
-#         ws_connect = websocket.websocket_connect(self._url)
-#         ws_connect.add_done_callback(self._connect_callback)
-#
-#     def send(self, data):
-#         """Send message to the server
-#         :param str data: message.
-#         """
-#         if not self._ws_connection:
-#             raise RuntimeError('No available websocket connection')
-#
-#         #self._ws_connection.write_message(escape.utf8(json.dumps(data)))
-#         self._ws_connection.write_message(f"Hello world, {data}")
-#
-#     def close(self):
-#         """Close connection."""
-#
-#         if not self._ws_connection:
-#             raise RuntimeError('Web socket connection is already closed.')
-#
-#         self._ws_connection.close()
-#
-#     def _connect_callback(self, future):
-#         """This is called after the connection future is resolved (whether succcesful or not)"""
-#         if future.exception() is None:
-#             self._ws_connection = future.result()
-#             self._on_connection_success()
-#             self._read_messages()
-#         else:
-#             self._on_connection_error(future.exception())
-#
-#     @gen.coroutine
-#     def _read_messages(self):
-#         """This method keeps checking for messages on the websocket"""
-#         while True:
-#             msg = yield self._ws_connection.read_message()
-#             if msg is None:
-#                 self._on_connection_close()
-#                 break
-#
-#             self._on_message(msg)
-#
-#     def _on_message(self, msg):
-#         """This is called when new message is available from the server.
-#         :param str msg: server message.
-#         """
-#         print(f"Received new message from the server: {msg}")
-#         try:
-#             message_object = json.loads(msg)
-#             if message_object["type"] == "featureMap":
-#                 self.update_feature_map(message_object)
-#             elif message_object["type"] == "start":
-#                 self.start_tracking(message_object)
-#             elif message_object["ty[e"] == "stop":
-#                 self.stop_tracking(message_object)
-#             else:
-#                 self.send("Unknown command")
-#         except ValueError:
-#             self.send("Bad json")
-#         except KeyError:
-#             self.send("Missed property in json")
-#
-#
-#     def _on_connection_success(self):
-#         """This is called on successful connection ot the server.
-#         """
-#         print(f"Connected to {self._url}")
-#
-#         self.send("Test")
-#
-#     def _on_connection_close(self):
-#         """This is called when server closed the connection.
-#         """
-#         print(f"Connection closed")
-#
-#     def _on_connection_error(self, exception):
-#         """This is called in case if connection to the server could
-#         not established.
-#         """
-#         raise Exception(f"Could not connect to {self._url}")
-#
-#     def update_feature_map(self, message):
-#         object_id = message["objectId"]
-#         feature_map = message["featureMap"]
-#         print(f"Someone send a feature map of an object with Id {object_id}")
-#
-#     def start_tracking(self, message):
-#         object_id = message["objectId"]
-#         frame_id = message["frameId"]
-#         box_id = message["boxId"]
-#         print(f'Someone wants to start tracking the object of bounding box {box_id} in frame {frame_id} with the new object id {object_id}')
-#
-#     def stop_tracking(self, message):
-#         object_id = message["objectId"]
-#         print(f'Someone wants to stop tracking the object {object_id}')
-#
-#
-#
-#
-
-# class TestWebSocketClient(WebSocketClient):
-#
-#     def _on_message(self, msg):
-#         print(msg)
-#         deadline = time.time() + 1
-#         ioloop.IOLoop().instance().add_timeout(
-#             deadline, functools.partial(self.send, str(int(time.time()))))
-#
-#     def _on_connection_success(self):
-#         print('Connected!')
-#         #self.send(str(int(time.time())))
-#
-#     def _on_connection_close(self):
-#         print('Connection closed!')
-#
-#     def _on_connection_error(self, exception):
-#         print('Connection error: %s', exception)
-
-
 import asyncio
 import json
 from tornado import websocket
@@ -154,12 +15,13 @@ def start_tracking(message):
     box_id = message["boxId"]
     print(f"Start tracking box {box_id} in frame_id {frame_id} with new object id {object_id}")
 
+
 def stop_tracking(message):
     object_id = message["objectId"]
     print(f"Stop tracking object {object_id}")
 
 
-def handle_msg(message):
+def read_msg(message):
     print("Server sent: " + message)
     try:
         message_object = json.loads(message)
@@ -187,14 +49,24 @@ def handle_msg(message):
         print("Someone missed a property in their json")
 
 
+# Write messages to the connection
+async def write_message(msg):
+    await connection.write_message(msg)
+
 
 async def main():
-    connection = await websocket.websocket_connect('ws://localhost:8000/processor', on_message_callback=handle_msg)
+    global connection
+    connection = await websocket.websocket_connect('ws://localhost:8000/processor', on_message_callback=read_msg)
 
     # Video processing loop - should we send bounding boxes here?
     while True:
-        await connection.write_message('{"type":"test", "frameId": 12, "boxId": 18}') # Don't await? don't want to block on this
-        await asyncio.sleep(100)  # Simulate delay
+        # non blocking call to write message in parallel
+        asyncio.get_event_loop().create_task(write_message('{"type":"test", "frameId": 12, "boxId": 18}'))
+
+        # Simulate delay
+        await asyncio.sleep(100)
 
 if __name__ == '__main__':
     asyncio.run(main())
+
+connection = None
