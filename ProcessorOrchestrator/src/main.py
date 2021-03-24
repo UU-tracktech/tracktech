55dd5b732e0a6245bd03aec61507980837e3c826
Rienk <rienkfidder@gmail.com>
2021-03-24 09:50:18 +0100

    FFT: Added SSL certificate support

--- ProcessorOrchestrator/src/main.py
+++ ProcessorOrchestrator/src/main.py
@@ -1,3 +1,5 @@
+import os
+import ssl
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.options import define, options
@@ -9,6 +11,11 @@ define('port', default=8000, help='port to listen on')
 
 
 def main():
+    # Get ssl ready, if provided in the environment variables
+    cert = os.environ.get('SSL_CERT')
+    key = os.environ.get('SSL_KEY')
+    use_tls = cert is not None and key is not None
+
     # Define socket for both client and processor
     handlers = [
         ('/client', ClientSocket),
@@ -19,9 +26,22 @@ def main():
     # Construct and serve the tornado application.
     app = Application(handlers)
 
-    http_server = HTTPServer(app)
-    http_server.listen(options.port)
-    print('Listening on port %i' % options.port)
+    if use_tls:
+        # Create a ssl context
+        ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        ssl_ctx.load_cert_chain(cert, key)
+
+        # Create a http server, with optional ssl.
+        http_server = HTTPServer(app, ssl_options=ssl_ctx)
+        http_server.listen(443)
+        print('listening over https')
+
+    else:
+        # Create a http server, with optional ssl.
+        http_server = HTTPServer(app)
+        http_server.listen(80)
+        print('listening over http')
+
     IOLoop.current().start()
 
 
