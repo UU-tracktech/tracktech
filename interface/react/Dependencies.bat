04792740b923729ba21a2e6850b49a47d80f5892
Brian <brianvanbeusekom@hotmail.com>
2021-03-25 14:42:23 +0100

    FIX dependency jest version

--- interface/react/Dependencies.bat
+++ interface/react/Dependencies.bat
@@ -1,5 +1,5 @@
 call npm install videojs
-call npm install -f jest@26.6.0
+call npm install -f jest
 call npm install -f jest babel-jest @babel/preset-env
 call npm install -f --save chokidar
 call npm install react-use-websocket
