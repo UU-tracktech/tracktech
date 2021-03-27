52e550250406a61f3031c357ba99a0d6a1602c1e
Max van der Hart <m.vanderhart@students.uu.nl>
2021-03-27 18:51:31 +0100

    UFT: Mock feature maps message

--- CameraProcessor/client.py
+++ CameraProcessor/client.py
@@ -7,6 +7,7 @@ from detection.dectection_obj import DetectionObj
 from input.hls_stream import HlsCapture
 import cv2
 from datetime import datetime
+import random
 
 # Setup (basic) logging
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
@@ -17,6 +18,7 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 frame_nr = 0
+feature_map_delay = 10
 capture = HlsCapture()
 
 
@@ -105,11 +107,27 @@ async def connect_to_url():
             logging.warning(f"Could not connect to {url}, trying again in 1 second...")
             await asyncio.sleep(1)
 
-def mock_detection_object():
-    obj = DetectionObj(datetime.now(), None, frame_nr)
+def mock_detection_object(frm):
+    obj = DetectionObj(datetime.now(), frm, frame_nr)
     obj.bounding_box = obj.mock_bounding_boxes()
-    print(obj.bounding_box[0].to_json())
     return obj
+
+
+mock_feature_map_dict = {
+    "type": "featureMaps"
+}
+
+# Update one of the mock feature maps in the mock dictionary
+def update_feature_maps():
+    object_id = random.randint(1,10)
+
+    # if key does not yet exist
+    if not object_id in mock_feature_map_dict:
+
+        # Create a mock feature map for the object
+        mock_feature_map_dict[object_id] = { "state": 0}
+
+    mock_feature_map_dict[object_id]["state"] = mock_feature_map_dict[object_id]["state"] + 1
 
 async def main():
     global connection, connected, connect_task_created, frame_nr
@@ -134,17 +152,24 @@ async def main():
         print(frame_nr)
 
         # Create detectionObj
-        detection_obj = DetectionObj(datetime.now(), frame, frame_nr)
+        #detection_obj = DetectionObj(datetime.now(), frame, frame_nr)
+        detection_obj = mock_detection_object(frame)
         # Run detection on object
-
 
         # Visualise rectangles and show it
         detection_obj.draw_rectangles()
         cv2.imshow('Frame', detection_obj.frame)
 
-        # Non-blocking call to write message in parallel, normally happens after frame processing
-        asyncio.get_event_loop().create_task(write_message('{"type":"test", "frameId": 12, "boxId": 18}'))
-        print(mock_detection_object().to_json())
+        # Tracking phase
+        update_feature_maps()
+
+        # Non-blocking call to send bounding boxes
+        asyncio.get_event_loop().create_task(write_message(detection_obj.to_json()))
+
+        # Every 10 frames, send updated feature maps
+        if frame_nr % feature_map_delay == 0:
+            print("Sending feature maps")
+            asyncio.get_event_loop().create_task(write_message(json.dumps(mock_feature_map_dict)))
 
         # Close loop when q is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
