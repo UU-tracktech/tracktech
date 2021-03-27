7cd8910733affe6eb632a914c2e19c4eaf808f19
saberzero1 <ewjbangma@hotmail.com>
2021-03-27 13:38:52 +0100

    CRF Refactored bounding_box unit tests
    Former-commit-id: 1f7ecfe9f80e68ffb3ad6d440c93a37d512ea84b

--- CameraProcessor/tests/unittests/detectiontests/test_bounding_box.py
+++ CameraProcessor/tests/unittests/detectiontests/test_bounding_box.py
@@ -1,7 +1,7 @@
 """Tests for bounding_box.py.
 
 Test different inputs in the class
- - Test if every type is correct.   
+ - Test if every type is correct.
  - Test if every value is correct.
  - Test empty inputs.
  - Test exceptions.
@@ -14,92 +14,93 @@ from detection.bounding_box import BoundingBox
 
 class TestBoundingBox:
 
+    # Setup
     def setup_method(self):
         self.data = BoundingBox(1, [0, 0, 1, 1], "person", 0.5)
-        self.unit_identifier = self.data.identifier
-        self.unit_rectangle = self.data.rectangle
-        self.unit_feature = None
-        self.unit_classification = self.data.classification
-        self.unit_certainty = self.data.certainty
+        self.identifier = self.data.identifier
+        self.rectangle = self.data.rectangle
+        self.feature = None
+        self.classification = self.data.classification
+        self.certainty = self.data.certainty
 
     # Testing typechecking
     def test_type_identifier(self):
-        assert isinstance(self.unit_identifier,
-                          type(self.unit_identifier))
+        assert isinstance(self.identifier,
+                          type(self.identifier))
 
     def test_type_rectangle(self):
-        assert isinstance(self.unit_rectangle,
-                          type(self.unit_rectangle))
+        assert isinstance(self.rectangle,
+                          type(self.rectangle))
 
     def test_type_feature(self):
-        assert isinstance(self.unit_feature,
-                          type(self.unit_feature))
+        assert isinstance(self.feature,
+                          type(self.feature))
 
     def test_type_classification(self):
-        assert isinstance(self.unit_classification,
-                          type(self.unit_classification))
+        assert isinstance(self.classification,
+                          type(self.classification))
 
     def test_type_certainty(self):
-        assert isinstance(self.unit_certainty,
-                          type(self.unit_certainty))
+        assert isinstance(self.certainty,
+                          type(self.certainty))
 
     # Testing empty fields that can be empty
     def test_empty_identifier(self):
-        assert self.unit_identifier is not None
+        assert self.identifier is not None
 
     def test_empty_rectangle(self):
-        assert self.unit_rectangle is not None
+        assert self.rectangle is not None
 
     @pytest.mark.skip(reason="feature attribute is currently unused")
     def test_empty_feature(self):
-        assert self.unit_feature is not None
+        assert self.feature is not None
 
     def test_empty_classification(self):
-        assert self.unit_classification is not None
+        assert self.classification is not None
 
     def test_empty_certainty(self):
-        assert self.unit_certainty is not None
+        assert self.certainty is not None
 
     # Testing exceptions
     def test_exception_identifier(self):
         with pytest.raises(Exception):
-            assert str(self.unit_identifier) == 'one'
+            assert str(self.identifier) == 'one'
 
     def test_exception_rectangle(self):
         with pytest.raises(Exception):
-            assert str(self.unit_rectangle) == [1]
+            assert str(self.rectangle) == [1]
 
     def test_exception_feature(self):
         with pytest.raises(Exception):
-            assert str(self.unit_feature) == 'some invalid feature'
+            assert str(self.feature) == 'some invalid feature'
 
     def test_exception_classification(self):
         with pytest.raises(Exception):
-            assert str(self.unit_classification) == 9
+            assert str(self.classification) == 9
 
     def test_exception_certainty(self):
         with pytest.raises(Exception):
-            assert str(self.unit_certainty) == 'some invalid value'
+            assert str(self.certainty) == 'some invalid value'
 
     # Testing values
     def test_value_identifier(self):
-        assert self.unit_identifier == 1
+        assert self.identifier == 1
 
     def test_value_rectangle(self):
-        assert self.unit_rectangle == [0, 0, 1, 1]
+        assert self.rectangle == [0, 0, 1, 1]
 
     def test_value_feature(self):
-        assert self.unit_feature is None
+        assert self.feature is None
 
     def test_value_classification(self):
-        assert self.unit_classification == "person"
+        assert self.classification == "person"
 
     def test_value_certainty(self):
-        assert self.unit_certainty == 0.5
+        assert self.certainty == 0.5
 
     # Testing others
     def test_length_rectangle(self):
-        assert len(self.unit_rectangle) == 4
+        assert len(self.rectangle) == 4
 
 
 if __name__ == '__main__':
