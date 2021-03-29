4d29f6f405efe7906ad5a0c30a1d9e0eafcc732e
J.Teeuwissen <j.teeuwissen@students.uu.nl>
2021-03-29 20:36:53 +0200

    FFT: tiny display
    Former-commit-id: bdcdb1b35aee16ec6a1d55e0c0aa15f2ce239777

--- interface/react/src/components/canvas.tsx
+++ interface/react/src/components/canvas.tsx
@@ -4,32 +4,27 @@ import { Queue } from 'queue-typescript'
 import { Box } from '../classes/ClientMessage'
 import { websocketContext } from '../components/websocketContext'
 
-export type canvasProps = { cameraId: number, width: number, height: number }
-export class Canvas extends React.Component<canvasProps> {
+export type canvasProps = { cameraId: number, width: number, height: number, onClickCallback: (id?: number) => void }
+type canvasState = { boxes: Box[] }
+export class Canvas extends React.Component<canvasProps, canvasState> {
 
     canvas = React.createRef<HTMLCanvasElement>()
-    queue = new Queue<Box>()
+    queue = new Queue<Box[]>()
 
     static contextType = websocketContext
     context!: React.ContextType<typeof websocketContext>
 
+    constructor(props: any) {
+        super(props)
+        this.state = { boxes: [] }
+    }
+
     componentDidMount() {
-        this.context.addListener(this.props.cameraId, (boxes: Box[]) => this.drawBoxes(boxes))
+        this.context.addListener(this.props.cameraId, (boxes: Box[]) => {
+            this.setState({ boxes: boxes })
+        })
     }
 
-    drawBoxes(boxes: Box[]) {
-        var canvas = this.canvas.current
-        if (canvas) {
-            var ctx = canvas.getContext('2d')
-            if (ctx) {
-                this.clear(ctx)
-
-                boxes.forEach(box => {
-                    if (ctx) this.drawBox(ctx, box)
-                })
-            }
-        }
-    }
     /*  enqueue(message: ClientMessage) {
          this.queue.enqueue(message)
          this.setState({ queueLength: this.queue.length })
@@ -47,58 +42,31 @@ export class Canvas extends React.Component<canvasProps> {
         this.setState({ queueLength: this.queue.length })
     }*/
 
-    clear(ctx: CanvasRenderingContext2D) {
-        ctx.clearRect(0, 0, this.props.width, this.props.height)
-    }
-
-    drawBox(ctx: CanvasRenderingContext2D, box: Box) {
-        var canvas = this.canvas.current
-
-        if (canvas) {
-            var rect = canvas.getBoundingClientRect()
-            var x1 = rect.width * box.x1
-            var y1 = rect.width * box.y1
-            var x2 = rect.width * box.x2
-            var y2 = rect.width * box.y2
-
-            this.clear(ctx)
-            this.drawRectangle(ctx, x1, y1, x2, y2)
-        }
-    }
-
-    drawRectangle(ctx: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number, type: number = 0) {
-        const colordict = { 0: 'red', 1: 'green', 2: 'blue' }
-
-        ctx.strokeStyle = colordict[type]
-        ctx.strokeRect(x1, y1, x2 - x1, y2 - y1)
-    }
-
-    handleMouseDown(event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
-        var canvas = this.canvas.current
-
-        if (canvas) {
-            var rect = canvas.getBoundingClientRect()
-            var x = event.clientX - rect.left
-            var y = event.clientY - rect.top
-
-            var ctx = canvas.getContext('2d')
-            if (ctx) {
-                this.clear(ctx)
-                this.drawRectangle(ctx, x - 10, y - 10, x + 10, y + 10, 1)
-            }
-        }
-    }
 
     render() {
+        const colordict = { 0: 'red', 1: 'green', 2: 'blue' }
         return <div>
-            < canvas
+            <div style={{ position: 'absolute', width: this.props.width, height: this.props.height, overflow: 'hidden' }}>
+                {
+                    this.state.boxes.map((box) => <div key={box.type} style={
+                        {
+                            position: 'relative',
+                            left: box.x1 * this.props.width, top: box.y1 * this.props.height, width: (box.x2 - box.x1) * this.props.width, height: (box.y2 - box.y1) * this.props.height,
+                            borderColor: colordict[box.type ?? 0], borderStyle: 'solid',
+                            transitionProperty: 'all', transitionDuration:'1s',
+                            zIndex: 1000
+                        }
+                    } onClick={() => this.props.onClickCallback(box.id)} />)
+                }
+            </div>
+            {/*  < canvas
                 key={this.props.cameraId}
                 style={{ position: 'absolute', zIndex: 1000 }}
                 width={this.props.width}
                 height={this.props.height}
                 ref={this.canvas}
-                onClick={(event) => this.handleMouseDown(event)}
-            />
+                onClick={(event) => this.onClick(event)}
+            /> */}
             <div>
                 {this.props.children}
             </div>
