3d118e0c0d798b0ab915167efc302d7a398c9901
Marieke van der Plaat <m.d.vanderplaat@uu.nl>
2021-03-29 17:52:56 +0200

    CRF contest.py in capture for global capture variables needed in capture tests.

---
+++ CameraProcessor/tests/unittests/capture/conftest.py
@@ -0,0 +1,27 @@
+import os
+
+import pytest
+
+from src.input.cam_capture import CamCapture
+from src.input.hls_capture import HlsCapture
+from src.input.image_capture import ImageCapture
+
+
+def __get_images_dir():
+    __root_dir = os.path.abspath(__file__ + '../../../../../')
+    __folder_name = 'test'
+    __images_dir = f'{__root_dir}\\data\\annotated\\{__folder_name}\\img1'
+    return __images_dir
+
+
+@pytest.fixture(params=[ImageCapture(__get_images_dir()), HlsCapture(), CamCapture()])
+def capture_implementation(request):
+    """ Defines capture_implementation as multiple implementations of iCapture, to be use in generic capture tests.
+
+    Args:
+        request: different implementations of capture.
+
+    Returns: implementation of capture.
+
+    """
+    return request.param
