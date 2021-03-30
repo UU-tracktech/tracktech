baf8902faf0a426f98f86021d867330a3b091124
saberzero1 <ewjbangma@hotmail.com>
2021-03-30 16:10:58 +0200

    UFT Working on Fix  WebSocket Unit Test
    Former-commit-id: b6090a6ae533fe366032eb6bdf02e7bd0a42b424

--- CameraProcessor/tests/unittests/test_websocket_client.py
+++ CameraProcessor/tests/unittests/test_websocket_client.py
@@ -1,6 +1,8 @@
 import pytest
 import json
 import src.websocket_client as websocket_client
+from src.websocket_client import WebsocketClient
+ws_client = WebsocketClient
 
 
 def __eq__(self, other):
@@ -23,10 +25,11 @@ class TestWebsocketClient:
 
     """
 
-    def setup_method(self):
+    def setup_method(self, ws_client):
         """Set ups mock messages for unit testing.
 
         """
+        self.data = WebsocketClient
         self.start_json = '{"type": "start", "objectId": 1, "frameId": 1, "boxId": 1}'
         self.stop_json = '{"type": "stop", "objectId": 1}'
         self.feature_map_json = '{"type": "featureMap", "objectId": 1, "featureMap": []}'
@@ -36,10 +39,11 @@ class TestWebsocketClient:
         self.message_object_start_tracking_string = self.start_json
         self.message_object_stop_tracking_string = self.stop_json
         self.message_object_update_feature_map_string = self.feature_map_json
-        self.connect = websocket_client.connected
-        self.connection = websocket_client.connection
-        self.connect_task_created = websocket_client.connect_task_created
-        self.write_message = websocket_client.write_message("test")
+        self.start_tracking = self.data.start_tracking(ws_client, self.message_object_start_tracking)
+        self.stop_tracking = self.data.stop_tracking(ws_client, self.message_object_stop_tracking)
+        self.update_feature_map = self.data.update_feature_map(ws_client, self.message_object_update_feature_map)
+        self.connect = self.data.connect
+        self.write_message = self.data._write_message(ws_client, "test")
 
     def test_start_tracking(self):
         """Checks if start_tracking takes correct values from message
@@ -62,40 +66,40 @@ class TestWebsocketClient:
         assert self.message_object_update_feature_map["objectId"] == 1
         assert self.message_object_update_feature_map["featureMap"] == []
 
-    def test_read_msg_start_tracking(self):
+    def test_read_msg_start_tracking(self, ws_client):
         """Checks if read_msg correctly parses start tracking message
 
         """
-        json_temp_start = websocket_client.read_msg(self.message_object_start_tracking_string)
+        json_temp_start = self.data._on_message(ws_client, self.message_object_start_tracking_string)
         assert json_temp_start.__eq__(self.message_object_start_tracking)
 
-    def test_read_msg_stop_tracking(self):
+    def test_read_msg_stop_tracking(self, ws_client):
         """Checks if read_msg correctly parses stop tracking message
 
         """
-        json_temp_stop = websocket_client.read_msg(self.message_object_stop_tracking_string)
+        json_temp_stop = ws_client._on_message(self.message_object_stop_tracking_string)
         assert json_temp_stop.__eq__(self.message_object_stop_tracking)
 
-    def test_read_msg_update_feature_map(self):
+    def test_read_msg_update_feature_map(self, ws_client):
         """Checks if read_msg correctly parses update feature map message
 
         """
-        json_temp_feature_map = websocket_client.read_msg(self.message_object_update_feature_map_string)
+        json_temp_feature_map = ws_client._on_message(self.message_object_update_feature_map_string)
         assert json_temp_feature_map.__eq__(self.message_object_update_feature_map)
 
-    def test_read_msg_type_error_exception(self):
+    def test_read_msg_type_error_exception(self, ws_client):
         """Checks if read_msg raises exception with invalid input
 
         """
         with pytest.raises(Exception):
-            assert websocket_client.read_msg('"invalidJson": "yes"')
+            assert ws_client._on_message(self, '"invalidJson": "yes"')
 
-    def test_read_msg_key_error_exception(self):
+    def test_read_msg_key_error_exception(self, ws_client):
         """Checks if read_msg raises exception with missing value in input
 
         """
         with pytest.raises(Exception):
-            assert websocket_client.read_msg('{"type": "start", "objectId": 1, "frameId": 1}')
+            assert ws_client._on_message(self, '{"type": "start", "objectId": 1, "frameId": 1}')
 
 
 if __name__ == '__main__':
