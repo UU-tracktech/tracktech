f246b89fa30e717b2a130c41a945954b98398feb
Max van der Hart <m.vanderhart@students.uu.nl>
2021-03-30 10:45:29 +0200

    DOC: Added docstrings to bounding box and detection object

--- CameraProcessor/detection/bounding_box.py
+++ CameraProcessor/detection/bounding_box.py
@@ -1,6 +1,9 @@
 import json
 
 class BoundingBox:
+    """
+    Contains information about a single bounding box
+    """
     def __init__(self, identifier, rectangle, classification, certainty):
         self.identifier = identifier
         self.rectangle = rectangle
@@ -9,6 +12,11 @@ class BoundingBox:
         self.certainty = certainty
 
     def to_json(self):
+        """
+        Converts the object to JSON format
+        Returns: JSON representation of the object
+
+        """
         str = json.dumps({
             "boxId": self.identifier,
             "rect": self.rectangle,
--- CameraProcessor/detection/dectection_obj.py
+++ CameraProcessor/detection/dectection_obj.py
@@ -3,6 +3,9 @@ import json
 from .bounding_box import BoundingBox
 
 class DetectionObj:
+    """
+    Contains all the bounding boxes for a specific frame
+    """
     def __init__(self, timestamp, frame, frame_nr):
         self.timestamp = timestamp
         self.frame = frame
@@ -10,6 +13,9 @@ class DetectionObj:
         self.bounding_box = []
 
     def draw_rectangles(self):
+        """
+        Draws the bounding boxes on the frame
+        """
         red = (255, 0, 0)
         for bounding_box in self.bounding_box:
             self.frame = cv2.rectangle(self.frame,
@@ -18,14 +24,12 @@ class DetectionObj:
                                        red,
                                        2)
 
-    def mock_bounding_boxes(self):
-        boxes = []
-        for i in range(1, 10):
-            boxes.append(BoundingBox(i, (0, 0, 10 * i, 10 * i), None, 0))
-        return boxes
-
-
     def to_json(self):
+        """
+        Converts the object to JSON format
+        Returns: JSON representation of the object
+
+        """
         return json.dumps({
             "type": "boundingBoxes",
             "frameId": self.frame_nr,
