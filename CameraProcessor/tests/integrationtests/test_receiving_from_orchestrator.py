97446046e0e90157587c92431de4a824803dccbf
saberzero1 <ewjbangma@hotmail.com>
2021-04-01 13:20:53 +0200

    FFT Connection tests done

--- CameraProcessor/tests/integrationtests/test_receiving_from_orchestrator.py
+++ CameraProcessor/tests/integrationtests/test_receiving_from_orchestrator.py
@@ -3,3 +3,6 @@
 # Expects to hear messages from orchestrator via other processor
 # Gets to hear feature map API calls from orchestrator when other processor sends them
 
+url = 'ws://processor-orchestrator-test-service/processor'
+url = 'ws://localhost:80/processor'
+websocket_test = WebsocketClient(url)
