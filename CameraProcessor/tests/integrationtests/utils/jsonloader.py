6281949ac44bbc72eb4fc226e4edba09638bf017
Jaimy Kwa <jaimykwa@gmail.com>
2021-04-02 16:03:41 +0200

    TST test_send_to_orchestrator is sorta working

--- CameraProcessor/tests/integrationtests/utils/jsonloader.py
+++ CameraProcessor/tests/integrationtests/utils/jsonloader.py
@@ -7,7 +7,7 @@ def load_data(datatype):
     """Load a JSON test data file of chosen type.
 
     Args:
-        A string, which is one of these options: {'boundingBoxes', 'start', 'stop', 'featureMap'}
+        A string, which is one of these options: {'boundingBoxes', 'start', 'stop', 'featureMap', 'invalid'}
 
     Returns:
         A dictionary of test data of the chosen JSON message type
@@ -16,13 +16,14 @@ def load_data(datatype):
         'boundingBoxes': 'boxes',
         'start': 'start',
         'stop': 'stop',
-        'featureMap': 'featuremaps'
+        'featureMap': 'featuremaps',
+        'invalid': 'invalid'
     }
     filename = switcher.get(datatype)
     if filename is None:
         raise NameError('The JSON object requested is not in scope.')
     else:
-        f = open(f'testdata/{filename}.json')
+        f = open(f'testdata/{filename}.json', encoding='utf8')
         return json.load(f)
 
 
@@ -30,7 +31,7 @@ def load_random_data(datatype, nr):
     """Load a random amount of JSON test data of chosen datatype
 
     Args:
-        - A string, which is one of these options: {'boundingBoxes', 'start', 'stop', 'featureMap'}
+        - A string, which is one of these options: {'boundingBoxes', 'start', 'stop', 'featureMap', 'invalid'}
         - The number of random data you want
 
     Returns:
@@ -41,13 +42,14 @@ def load_random_data(datatype, nr):
         'boundingBoxes': 'boxes',
         'start': 'stopstart',
         'stop': 'stopstart',
-        'featureMap': 'featuremaps'
+        'featureMap': 'featuremaps',
+        'invalid': 'invalid'
     }
     filename = switcher.get(datatype)
     if filename is None:
         raise NameError('The JSON object requested is not in scope.')
     else:
-        f = open(f'testdata/{filename}.json')
+        f = open(f'testdata/{filename}.json', encoding='utf8')
         jfile = json.load(f)
         d = []
         for i in range(nr):
