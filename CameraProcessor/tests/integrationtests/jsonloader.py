0002e57a133f0b3842dace601a5868ab5347d853
saberzero1 <ewjbangma@hotmail.com>
2021-04-02 13:02:30 +0200

    UFT Working on integration tests

--- CameraProcessor/tests/integrationtests/jsonloader.py
+++ CameraProcessor/tests/integrationtests/jsonloader.py
@@ -1,4 +1,6 @@
 import json
+import random
+import os
 
 
 def load_data(datatype):
@@ -22,3 +24,32 @@ def load_data(datatype):
     else:
         f = open(f'testdata/{filename}.json')
         return json.load(f)
+
+
+def load_random_data(datatype, nr):
+    """Load a random amount of JSON test data of chosen datatype
+
+    Args:
+        - A string, which is one of these options: {'boundingBoxes', 'start', 'stop', 'featureMap'}
+        - The number of random data you want
+
+    Returns:
+        A dictionary of length 'nr' of the chosen JSON message type
+    """
+
+    switcher = {
+        'boundingBoxes': 'boxes',
+        'start': 'stopstart',
+        'stop': 'stopstart',
+        'featureMap': 'featuremaps'
+    }
+    filename = switcher.get(datatype)
+    if filename is None:
+        raise NameError('The JSON object requested is not in scope.')
+    else:
+        f = open(f'testdata/{filename}.json')
+        jfile = json.load(f)
+        d = []
+        for i in range(nr):
+            d.append(random.choice(jfile))
+        return d
--- CameraProcessor/tests/integrationtests/test_receiving_from_orchestrator.py
+++ CameraProcessor/tests/integrationtests/test_receiving_from_orchestrator.py
@@ -14,19 +14,27 @@ from async_timeout import timeout
 # Expects to hear messages from orchestrator via other processor
 # Gets to hear feature map API calls from orchestrator when other processor sends them
 
-url = 'ws://processor-orchestrator-test-service/processor'
+# url = 'ws://processor-orchestrator-test-service/processor'
 url = 'ws://localhost:80/processor'
 
 
-def with_timeout(t):
-    def wrapper(corofunc):
-        async def run(*args, **kwargs):
-            with timeout(t):
-                return await corofunc(*args, **kwargs)
-        return run
-    return wrapper
+class TestReceivingFromOrchestrator:
+
+    def setup_method(self):
+        self.ws_client = WebsocketClient(url)
+
+    def with_timeout(t):
+        def wrapper(corofunc):
+            async def run(*args, **kwargs):
+                with timeout(t):
+                    return await corofunc(*args, **kwargs)
+
+            return run
+
+        return wrapper
 
 
 websocket_test = WebsocketClient(url)
 
-
+if __name__ == '__main__':
+    pytest.main(TestReceivingFromOrchestrator)
