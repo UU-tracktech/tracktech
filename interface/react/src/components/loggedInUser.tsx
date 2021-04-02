4b57ebf32314e1634e5d4e055137f2036b51b009
J.Teeuwissen <j.teeuwissen@students.uu.nl>
2021-04-02 08:25:10 +0200

    UFT: removed some code
    Former-commit-id: 73220d7f612f623ab39deaefcfc00eba504b15f4

--- interface/react/src/components/loggedInUser.tsx
+++ interface/react/src/components/loggedInUser.tsx
@@ -7,28 +7,12 @@ export const LoggedInUser = () => {
 
     //Obtain keycloak
     const { keycloak } = useKeycloak()
-
-    return(
+    console.log(keycloak)
+    return (
         <Navbar.Text>
             {
-                keycloak.authenticated ? <>Hallo {getUsername()}</> : <></>
+                keycloak.authenticated && keycloak.tokenParsed && <>Logged in as: {keycloak.tokenParsed['name']}</>
             }
         </Navbar.Text>
     )
-
-    /**Returns the first name of the logged in user */
-    function getUsername() : string {
-
-        //The token contains all info we could need
-        //First name, last name, email, etc
-        if(keycloak.tokenParsed) {
-            console.log('Token:', keycloak.tokenParsed)
-            return keycloak.tokenParsed['given_name']
-        }
-        else
-        {
-            console.log('Can\'t get logged in user')
-            return ''
-        }
-    }
 }
