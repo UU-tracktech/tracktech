52f0eec0c7e7c26a1c358eadd95f9b8aa90146f8
GerardvSchie <g.j.vanschie@students.uu.nl>
2021-04-03 12:28:17 +0200

    Doc: docstrings

--- CameraProcessor/src/pipeline/detection/bounding_box.py
+++ CameraProcessor/src/pipeline/detection/bounding_box.py
@@ -1,10 +1,19 @@
 import json
+from typing import List
+
 
 class BoundingBox:
+    """Contains information about a single bounding box
     """
-    Contains information about a single bounding box
-    """
-    def __init__(self, identifier, rectangle, classification, certainty):
+    def __init__(self, identifier: int, rectangle: List[int], classification: str, certainty: float):
+        """
+
+        Args:
+            identifier (int):
+            rectangle :
+            classification:
+            certainty:
+        """
         self.identifier = identifier
         self.rectangle = rectangle
         if len(rectangle) != 4:
@@ -13,9 +22,8 @@ class BoundingBox:
         self.classification = classification
         self.certainty = certainty
 
-    def to_json(self):
-        """
-        Converts the object to JSON format
+    def to_json(self) -> json:
+        """Converts the object to JSON format
         Returns: JSON representation of the object
 
         """
