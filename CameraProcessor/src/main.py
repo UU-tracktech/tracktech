e60ab949999491395f6f02fa7daebdf58e27c1d3
GerardvSchie <g.j.vanschie@students.uu.nl>
2021-04-05 16:12:16 +0200

    OTH: Docker ini path

--- CameraProcessor/src/main.py
+++ CameraProcessor/src/main.py
@@ -34,7 +34,10 @@ def main(args):
 
     # Load the config file, take the relevant Yolov5 section
     configs = configparser.ConfigParser(allow_no_value=True)
-    configs.read('../configs.ini')
+    # Docker
+    configs.read('configs.ini')
+    # Local run
+    # configs.read('../configs.ini')
     yolo_config = configs['Yolov5']
 
     # Instantiate the Detection Object
