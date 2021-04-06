18413c42ce7628189e67e1ab6d545ae60632be69
Max van der Hart <m.vanderhart@students.uu.nl>
2021-04-06 13:54:02 +0200

    FIX: Wrong draw method

--- CameraProcessor/src/pipeline/detection/detection_obj.py
+++ CameraProcessor/src/pipeline/detection/detection_obj.py
@@ -21,10 +21,10 @@ class DetectionObj:
         for bounding_box in self.bounding_boxes:
             height, width, _ = self.frame.shape
             self.frame = cv2.rectangle(self.frame,
-                                       (bounding_box.rectangle[0] * width,
-                                        bounding_box.rectangle[0] * height),
-                                       (bounding_box.rectangle[1] * width,
-                                        bounding_box.rectangle[2] * height),
+                                       (int(bounding_box.rectangle[0] * width),
+                                       int(bounding_box.rectangle[1] * height)),
+                                       (int(bounding_box.rectangle[2] * width),
+                                        int(bounding_box.rectangle[3] * height)),
                                        red,
                                        2)
 
