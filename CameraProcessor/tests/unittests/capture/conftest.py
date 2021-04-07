b0f474441d3fbd58963ab462ca70f1b3d8a62ccb
GerardvSchie <g.j.vanschie@students.uu.nl>
2021-04-07 09:15:18 +0200

    TST: Remove documentation stage

--- CameraProcessor/tests/unittests/capture/conftest.py
+++ CameraProcessor/tests/unittests/capture/conftest.py
@@ -15,7 +15,6 @@ def __get_images_dir():
     """
     __root_dir = os.path.abspath(__file__ + '../../../../../')
     __folder_name = 'test'
-    # __images_dir = f'{__root_dir}\\data\\annotated\\{__folder_name}\\img1'
     __images_dir = f'{__root_dir}/data/annotated/test/img1'
     return __images_dir
 
