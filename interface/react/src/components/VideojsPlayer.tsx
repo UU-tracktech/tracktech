1e328f036aa5271c31f4310a227ad6aa01171e7f
Brian <brianvanbeusekom@hotmail.com>
2021-04-08 18:11:15 +0200

    UFT: timestamp for frameID
    It should work as expected now?

--- interface/react/src/components/VideojsPlayer.tsx
+++ interface/react/src/components/VideojsPlayer.tsx
@@ -7,9 +7,11 @@ export class VideoPlayer extends React.Component<VideoPlayerProps> {
     private player?: videojs.Player
     private videoNode?: HTMLVideoElement
 
-    //Offset used for the time based on fragment number
-    private offset = 0
-    private startUri;
+    private initialUriInterval //Interval ID
+    private changeInterval //Interval ID
+    private updateInterval //Interval ID
+    private startUri    //The first URI the player gets
+    private startTime   //The timestamp where the player started
 
     componentDidMount() {
         // instantiate video.js
@@ -20,68 +22,101 @@ export class VideoPlayer extends React.Component<VideoPlayerProps> {
             this.player?.on('playerresize', () => this.onResize())
             this.player?.on('play', () => this.onResize())
 
-            this.player?.on('play', () => {
-                this.getCurrentTime()
-                let uriCheck = this.player?.setInterval(() => {
-                    let playerHLS = (this.player?.tech() as any).vhs
-                    let media = playerHLS.playlists.media()
+            //Timestamp stuff
 
-                    let currentUri = media.segments[0].uri
-
-                    if (currentUri !== this.startUri) {
-                        let splitUri = currentUri.split('_V')[1]
-                        let fragNum = splitUri.slice(1, splitUri.length - 3)
-                        const segmentTimestamp: number = (parseInt(fragNum) - 1) * 2
-                        this.offset = segmentTimestamp - 1
-                        this.clearInterval(uriCheck)
-                        this.player?.currentTime(0)
-                        //console.log('Interval done')
-                    }
-                }, 10)
+            /* On the first time a stream is started, attempt getting the
+               URI and keep going with the interval until one is obtained */
+            this.player?.on('firstplay', () => {
+                this.initialUriInterval = this.player?.setInterval(() => {
+                    this.getInitialUri()
+                }, 200)
             })
 
-            this.player?.setInterval(() => {
+            this.player?.on('play', () => {
+                this.updateInterval = this.player?.setInterval(() => {
+                    this.updateTimestamp()
+                }, 100)
+            })
 
-                let playerTime = this.player?.currentTime();
-
-                if(playerTime) {
-
-                    // console.log('playertime', playerTime)
-                    // console.log('offset', this.offset)
-                    let stamp = playerTime + this.offset
-
-                    let min = Math.floor(stamp / 60)
-                    let sec = Math.floor(stamp % 60)
-                    let toPrint = min + ':' + sec
-
-                    console.log('time', toPrint)
-                }
-            }, 1000/24)
+            /* Every time the stream is paused we can stop updating the
+            * interval, player.currentTime() will keep going in the
+            * background anyway */
+            this.player?.on('pause', () => {
+                this.player?.clearInterval(this.updateInterval)
+            })
         })
     }
 
-    clearInterval(x) {
-        this.player?.clearInterval(x)
+    /**
+     * Accesses the video player tech and returns the URI
+     * from the first segment in the playlist
+     */
+    getURI() : string | undefined {
+        try {
+            //passing any argument suppresses a warning about
+            //accessing the tech
+            let tech = this.player?.tech({randomArg: true})
+            if(tech)
+                return tech['vhs'].playlists.media().segments[0].uri
+        } catch (e) {
+            console.warn(e)
+            return undefined
+        }
     }
 
-    getCurrentTime() {
-        let playerHLS = (this.player?.tech() as any).vhs
-        //console.log('playerhls', playerHLS)
+    /**
+     * Used in an interval, attempts to get an URI and
+     * once it has one cancels itself, and starts a new
+     * interval that waits for a change in URI
+     */
+    getInitialUri() {
+        let currentUri = this.getURI()
+        if(currentUri) {
+            console.log('InitialURI: ', currentUri)
 
-        let media = playerHLS.playlists.media()
-        let lastUri = media.segments[0].uri
-        this.startUri = lastUri
+            this.startUri = currentUri
+            this.player?.clearInterval(this.initialUriInterval)
+            this.changeInterval = this.player?.setInterval(() => {
+                this.lookForUriUpdate()
+            }, 1000/24)
+        }
+    }
 
-        let splitUri = lastUri.split('_V')[1]
-        let fragNum = splitUri.slice(1, splitUri.length - 3)
-        const segmentTimestamp = (parseInt(fragNum) - 1) * 2
-        this.offset = segmentTimestamp
+    /**
+     * Used in an interval, attempts to get the URI of the current
+     * segment, and compares this to the initial URI. If it is not
+     * the same, cancels the interval and sets the time that
+     * the timestamp will be based on
+     */
+    lookForUriUpdate() {
+        let currentUri = this.getURI()
+        if(currentUri !== this.startUri) {
+            //ensure it is a string because typescript
+            if(typeof currentUri === 'string') {
+                console.log('URI changed: ', currentUri)
+                this.startTime = GetSegmentStarttime(currentUri)
+                console.log('Starttime: ', CreateTimestamp(this.startTime))
+                this.player?.clearInterval(this.changeInterval)
+            }
+        }
+    }
 
-        // let min = Math.floor(segmentTimestamp / 60)
-        // let sec = segmentTimestamp % 60
-        // let stamp = min + ':' + sec
-        //console.log('timestamp', stamp)
-        //console.log('currentTime', this.player?.currentTime())
+    /**
+     * Should be done in an interval, started whenever the user hits play
+     * this interval should be stopped whenever the user pauses
+     */
+    updateTimestamp() {
+
+        if(!this.startTime) {
+            console.log('Timestamp: Loading...')
+            return
+        }
+
+        let currentPlayer = this.player?.currentTime()
+        //dont ask why -4, it just works
+        let timeStamp = CreateTimestamp(this.startTime + currentPlayer - 4)
+
+        console.log('Timestamp: ', timeStamp)
     }
 
     onResize() {
@@ -143,4 +178,46 @@ class ToggleSizeButton extends videojs.getComponent('Button') {
     public handleClick(_e) {
         this.onClick()
     }
+}
+
+/**
+ * Takes a timestamp in seconds and converts it to the format
+ * mm:ss:ms
+ * @param {number} time The time in seconds
+ * @returns {string} The time formatted as mm:ss:ms
+ */
+function CreateTimestamp(time : number) : string {
+
+    let min = Math.floor(time / 60)
+    //toFixed(1) makes it so it is rounded to 1 decimal
+    let sec = (time % 60).toFixed(1)
+    //to make it look pretty
+    if(parseFloat(sec) < 10)
+        sec = '0' + sec
+    return min + ':' + sec
+}
+
+/**
+ * Takes the filename of a segment of the stream and
+ * determines the time of the video when this segment started
+ * @param {string} segName The filename of the segment
+ * @returns {number} The time in seconds
+ */
+function GetSegmentStarttime(segName : string) : number {
+
+    //Assuming the forwarder will always send a stream using
+    //HLS, which gives .ts files afaik
+    if(!segName.endsWith('.ts')){
+        console.warn('GetSegmentStarttime: ' +
+            'expected .ts file but got something else')
+        return NaN
+    }
+
+    //filename ends with _VXYYY.ts where X is a version
+    //and YYY is the segment number
+    let end = segName.split('_V')[1]
+    let number = end.slice(1, end.length-3)
+    //Every segment is 2 seconds, therefore
+    //the number * 2 is the timestamp
+    return parseInt(number) * 2
 }
