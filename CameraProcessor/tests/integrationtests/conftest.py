0505c635e157a5d95c86d8921da60a4a6531df16
Jaimy Kwa <jaimykwa@gmail.com>
2021-04-09 12:23:58 +0200

    CRF/TST Minor changes, added systemtest data file
    This has gotten out of hand since I started making ONE file out of boredom
    so here's the full changelist since this is so unorthodox:
    - Added systemtests folder to tests folder
    - Added vidstream_to_json.py to systemtests, which currently runs yolov5 on a 10-ish second video and writes the resulting JSON messages to a file
    - Modified conftest in integrationtests folder to actually hold the two fixtures used. I mean c'mon guys
    - Modified videocapture object to work again
    
    
    Former-commit-id: a6ec90921162b952999c12bedb4bd725fb72548e

--- CameraProcessor/tests/integrationtests/conftest.py
+++ CameraProcessor/tests/integrationtests/conftest.py
@@ -1,3 +1,20 @@
 import sys
 import os
+import pytest
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+
+@pytest.fixture(params=['boundingBoxes', 'start', 'stop', 'featureMap', 'invalid', 'bad'])
+def message_type(request):
+    """Fixture to generate message types
+
+    """
+    return request.param
+
+
+@pytest.fixture(params=[(1, True), (10, True), (999, False)], ids=["1, True", "10, True", "999, False"])
+def amount(request):
+    """Fixture to generate message amounts and whether or not invalid messages
+
+    """
+    return request.param
