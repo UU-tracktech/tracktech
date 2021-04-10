372198d063d8e9ef0732ec8c42063b963cc37024
Max van der Hart <m.vanderhart@students.uu.nl>
2021-04-10 14:27:13 +0200

    FFT: Multistage docker
    Former-commit-id: ef1f1f8ab2ece30cf1bd8894e93e83bac40fb05e

--- CameraProcessor/rebuild.bat
+++ CameraProcessor/rebuild.bat
@@ -5,13 +5,13 @@ set containerName=camera-processor-yolov5-container
 docker rm -f %containerName%
 
 :: Create new image
-docker build -t %imageName% .
+docker build --target=build -t %imageName% .
 
 :: Remove dangling images
 docker image prune -f
 
 :: Run new container...
-docker run -t -d --gpus all --name %containerName% %imageName%
+docker run -t -d --gpus all  --name %containerName% %imageName%
 :: docker run -t -d --name %containerName% %imageName%
 
 Pause
