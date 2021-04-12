648151b8a08febc729992e5a744bb716375be9e9
saberzero1 <ewjbangma@hotmail.com>
2021-04-12 15:42:14 +0200

    TST Docstring test
    Former-commit-id: 121f32bfae282889bad33364210affe8019256e1

--- pylint_ignore.py
+++ pylint_ignore.py
@@ -1,16 +1,31 @@
+"""Import utils to be able to give proper commands to pylint
+
+"""
 from pylint.utils import utils
 
 
 class PylintIgnorePaths:
+    """Patterns to ignore when linting
+
+    """
     def __init__(self, *paths):
+        """Gets paths
+
+        """
         self.paths = paths
         self.original_expand_modules = utils.expand_modules
         utils.expand_modules = self.patched_expand
 
     def patched_expand(self, *args, **kwargs):
+        """Get correct filepaths for linting
+
+        """
         result, errors = self.original_expand_modules(*args, **kwargs)
 
         def keep_item(item):
+            """Throws out all ignored file paths and subpaths
+
+            """
             if any(1 for path in self.paths if item['path'].startswith(path)):
                 return False
 
