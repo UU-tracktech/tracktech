0d82da10367f98557fbf91ecbf5aba2d9f64a9de
saberzero1 <ewjbangma@hotmail.com>
2021-04-15 23:44:18 +0200

    RFC: Code refactor

--- utility/pylint_ignore.py
+++ utility/pylint_ignore.py
@@ -16,20 +16,20 @@ class PylintIgnorePaths:
         self.original_expand_modules = pylintutils.expand_modules
         pylintutils.expand_modules = self.patched_expand
 
+    def keep_item(self, item):
+        """Throws out all ignored file paths and subpaths
+
+        """
+        if any(1 for path in self.paths if item['path'].startswith(path)):
+            return False
+
+        return True
+
     def patched_expand(self, *args, **kwargs):
         """Get correct filepaths for linting
 
         """
         result, errors = self.original_expand_modules(*args, **kwargs)
-
-        def keep_item(item):
-            """Throws out all ignored file paths and subpaths
-
-            """
-            if any(1 for path in self.paths if item['path'].startswith(path)):
-                return False
-
-            return True
 
         result = list(filter(keep_item, result))
 
