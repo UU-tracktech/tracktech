ebcdbbab79524fc19bca0c1c2b2a246696ce09d8
saberzero1 <ewjbangma@hotmail.com>
2021-04-16 00:03:54 +0200

    FIX: Ignore

--- utility/pylint_ignore.py
+++ utility/pylint_ignore.py
@@ -16,21 +16,12 @@ class PylintIgnorePaths:
         self.original_expand_modules = pylintutils.expand_modules
         pylintutils.expand_modules = self.patched_expand
 
-    def keep_item(self, item):
-        """Throws out all ignored file paths and subpaths
-
-        """
-        if any(1 for path in self.paths if item['path'].startswith(path)):
-            return False
-
-        return True
-
     def patched_expand(self, *args, **kwargs):
         """Get correct filepaths for linting
 
         """
         result, errors = self.original_expand_modules(*args, **kwargs)
 
-        result = list(filter(keep_item, result))
+        result = list(filter(not any(1 for path in self.paths if item['path'].startswith(path)), result))
 
         return result, errors
