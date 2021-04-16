1e6a78ba8dd0e0738e85678f66bcfb3d8aea2a2c
Jaimy Kwa <jaimykwa@gmail.com>
2021-04-16 14:29:39 +0200

    FIX No window opened during tests anymore
    Former-commit-id: 88aa1e74d9a97e18736b5ea3a9266c7171edb785

--- CameraProcessor/tests/unittests/pipeline/conftest.py
+++ CameraProcessor/tests/unittests/pipeline/conftest.py
@@ -2,13 +2,14 @@ import pytest
 
 from tests.unittests.utils.fake_websocket import FakeWebsocket
 
-"""Can't use fake client because Docker dislikes opencv
+"""Can't use None client because Docker dislikes using windows
 
 """
 # @pytest.fixture(params=[FakeWebsocket(), None], ids=["Fake Client", "None Client"])
 # def clients(request):
 #     return request.param
 
-@pytest.fixture(params=[None], ids=["No client"])
+
+@pytest.fixture(params=[FakeWebsocket()], ids=["Fake Client"])
 def clients(request):
     return request.param
