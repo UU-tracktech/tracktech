066e2896aea8d9064b0eea87e01d881259c0407f
Jaimy Kwa <jaimykwa@gmail.com>
2021-04-16 13:19:08 +0200

    TST added skips to the GPU using tests

--- CameraProcessor/tests/unittests/test_main.py
+++ CameraProcessor/tests/unittests/test_main.py
@@ -11,6 +11,7 @@ class TestProcessorMain:
 
     """
     @pytest.mark.timeout(30)
+    @pytest.mark.skip("YOLOv5 GPU Acceleration does not work yet in Docker")
     def test_run_main(self):
         p = EProcess(target=processor_main, args=([],))
         p.start()
