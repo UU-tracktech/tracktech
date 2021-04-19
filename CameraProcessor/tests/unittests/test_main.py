14e488032d129d7f6cd818bc90922c7ff5f75489
Jaimy Kwa <jaimykwa@gmail.com>
2021-04-19 13:17:50 +0200

    FIX More ugly commenting
    Former-commit-id: d2b88fc8ab7e4e165a3704acf31d5b5dddcdfe5d

--- CameraProcessor/tests/unittests/test_main.py
+++ CameraProcessor/tests/unittests/test_main.py
@@ -2,7 +2,7 @@ import pytest
 import logging
 import time
 
-from processor.main import main as processor_main
+# from processor.main import main as processor_main # More ugly commenting
 from tests.unittests.utils.exception_handler_process import EProcess
 
 
@@ -13,7 +13,8 @@ class TestProcessorMain:
     @pytest.mark.timeout(30)
     @pytest.mark.skip("YOLOv5 GPU Acceleration does not work yet in Docker")
     def test_run_main(self):
-        p = EProcess(target=processor_main, args=([],))
+        # p = EProcess(target=processor_main, args=([],))
+        p = EProcess(target=None)
         p.start()
         now = time.time()
         while now + 15 > time.time():
