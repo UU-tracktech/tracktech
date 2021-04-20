26b24659cfa0c99d5210b9dbb0a4a469a2e9539b
saberzero1 <ewjbangma@hotmail.com>
2021-04-20 21:26:46 +0200

    FIX: HLS timeout bug
    Former-commit-id: 587db2ba2d22148af2d51ca6dfb9d29cce4f5551

--- CameraProcessor/tests/unittests/capture/test_captures.py
+++ CameraProcessor/tests/unittests/capture/test_captures.py
@@ -29,8 +29,7 @@ class TestCaptures:
         """
         assert capture_implementation.get_next_frame()[0]
 
-    @pytest.mark.timeout(10)
-    @pytest.mark.skip(reason="Bugged for now")
+    @pytest.mark.timeout(20)
     def test_closed(self, capture_implementation):
         """Asserts capture to not be opened after calling closed.
 
