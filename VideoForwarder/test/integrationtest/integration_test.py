f2d1478ee197f7d59c65ecd57696e575659653b6
GerardvSchie <g.j.vanschie@students.uu.nl>
2021-04-20 23:50:33 +0200

    FIX: typo in command
    Former-commit-id: c3386311853a4e06cf22637d22d1bcc8ea6f2c66

--- VideoForwarder/test/integrationtest/integration_test.py
+++ VideoForwarder/test/integrationtest/integration_test.py
@@ -68,47 +68,79 @@ class TestVideoForwarder:
     def test_invalid_http_request(self, http_client):
         """Checks connection between forwarder and mock client with invalid url
 
+        Does not use pytest.raises since it cannot yield
+
         Args:
             http_client: Httpclient that connects
 
         """
 
-        # Fetch httpclient
+        # Create connection with invalid url
         try:
             yield http_client.fetch(self.camera_url)
             assert False
+        # Asserts exception is raised
         except Exception as e:
             assert True
 
     @pytest.mark.gen_test(timeout=15)
-    def test_cache_control_header(self, http_client):
+    def test_headers(self, http_client):
+        """Tests hls stream header on certain properties
+
+        Args:
+            http_client: Httpclient that connects
+
+        """
         response = yield http_client.fetch(self.camera_url)
         assert response.headers['Cache-control'] == 'no-store'
-
-    @pytest.mark.gen_test(timeout=15)
-    def test_access_control_allow_origin(self, http_client):
-        response = yield http_client.fetch(self.camera_url)
         assert response.headers['Access-Control-Allow-Origin'] == '*'
 
     @pytest.mark.gen_test(timeout=15)
     def test_generate_multiple_video_outputs(self, http_client):
+        """Tests whether the stream generates several headers
+
+        Args:
+            http_client: Httpclient that connects
+
+        """
+        # Create camera with versions
         camera = self.camera
         versions = self.camera_versions
         camera_versions = [camera + version for version in versions]
         version_counter = 0
+
+        # Create a connection
         yield http_client.fetch(self.camera_url)
+
+        # Foreach camera version
         for camera_version in camera_versions:
+            # List all files inside directory that start with current version
             for file in os.listdir(self.stream_dir):
                 if file.startswith(camera_version):
                     version_counter += 1
                     break
+
+        # Asserts number of versions is correct
         assert version_counter == len(versions)
 
     @pytest.mark.gen_test(timeout=15)
     def test_delete_files(self, http_client):
+        """Tests whether files are properly deleted after 61 seconds
+
+        Args:
+            http_client: Httpclient that connects
+
+        """
+        # Create camera
         camera = self.camera
+
+        # Create connection
         yield http_client.fetch(self.camera_url)
+
+        # After 60 seconds files are deleted on server
         time.sleep(61)
+
+        # Checks whether files are actually deleted
         for file in os.listdir(self.stream_dir):
             if file.startswith(camera):
                 assert False
