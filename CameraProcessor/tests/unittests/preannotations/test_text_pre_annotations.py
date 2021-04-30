f10e3bd65494b50d5eaf2426dd263c3c69f9b349
saberzero1 <ewjbangma@hotmail.com>
2021-04-30 13:13:01 +0200

    FFT: testing
    Former-commit-id: b596f49966f5bab2eb9ae45a744793459521f575

--- CameraProcessor/tests/unittests/preannotations/test_text_pre_annotations.py
+++ CameraProcessor/tests/unittests/preannotations/test_text_pre_annotations.py
@@ -8,6 +8,7 @@ Utrecht University within the Software Project course.
 import os
 import pytest
 from processor.training.pre_annotations import PreAnnotations
+from tests.unittests.preannotations.test_pre_annotations import example_text_file
 
 example_text_file = os.path.join(os.path.dirname(__file__),
                                  '../../../data/tests/unittests/preannotated/example_pre_annotations.txt')
