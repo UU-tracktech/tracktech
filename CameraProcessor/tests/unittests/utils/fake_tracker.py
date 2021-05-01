0690203ce93554d4869c9107bdd465b0860008a0
GerardvSchie <g.j.vanschie@students.uu.nl>
2021-05-01 10:32:58 +0200

    CRF: Restructure test files/folders and update licenses inside test files
    Former-commit-id: 4af4b1dba2859c50ec1799a04e4cc642a5308874

--- CameraProcessor/tests/unittests/utils/fake_tracker.py
+++ CameraProcessor/tests/unittests/utils/fake_tracker.py
@@ -1,5 +1,9 @@
 """Mock tracker for testing.
 
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+
 """
 import logging
 import random
