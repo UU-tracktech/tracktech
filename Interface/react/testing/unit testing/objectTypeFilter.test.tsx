6c2ccb064fdb6a06bff85040c4d2b9359c3b8a39
Brian <brianvanbeusekom@hotmail.com>
2021-05-05 17:30:37 +0200

    TST: filtering
    Former-commit-id: 7c18445589c6df92d295b6565f3f7c3f6b493d94

--- Interface/react/testing/unit testing/objectTypeFilter.test.tsx
+++ Interface/react/testing/unit testing/objectTypeFilter.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { screen, render } from '@testing-library/react'
+import { screen, render, getAllByTestId } from '@testing-library/react'
 import { ObjectTypeFilter } from '../../src/components/objectTypeFilter'
 
 test('it renders without error', () => {
@@ -12,7 +12,29 @@ test('it renders without error', () => {
   )
 })
 
-test.todo('the number of buttons correspond to the number of object types')
+test('the number of buttons correspond to the number of object types', () => {
+  render(
+    <ObjectTypeFilter
+      objectTypes={[
+        ['type1', false],
+        ['type2', false],
+        ['type3', false],
+        ['type4', false],
+        ['type5', false],
+        ['type6', false]
+      ]}
+      addHidden={jest.fn()}
+      removeHidden={jest.fn()}
+    />
+  )
+
+  //get the Card containing the buttons
+  const card = screen.getByTestId('filterCard')
+
+  //We passed a list of 6 elements, so the card should have 6 buttons as children
+  const children = getAllByTestId(card, 'filterButton')
+  expect(children.length).toBe(6)
+})
 
 describe('Button clicking tests', () => {
   it.todo('changes appearance on click')
