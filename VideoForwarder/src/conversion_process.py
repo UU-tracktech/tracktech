fa5eef20467d40f598ecc5bb30eae32fa1e0464e
J.Teeuwissen <j.teeuwissen@students.uu.nl>
2021-05-05 12:50:02 +0200

    UFT: start using 1:1, not tested yet.
    Former-commit-id: f8cce417717490504508bfb3a088a88fd6af08fb

--- VideoForwarder/src/conversion_process.py
+++ VideoForwarder/src/conversion_process.py
@@ -8,23 +8,21 @@ Utrecht University within the Software Project course.
 
 from subprocess import Popen
 
-from src.utils import get_stream_variables
+from src.stream_options import StreamOptions
 
 
-def get_conversion_process(ip_address, audio, camera, root):
+def get_conversion_process(url, audio, root, stream_options):
     """Gets the ffmpeg conversion command for the camera stream that can be started later on
 
     Args:
-        ip_address (str): Ip address of the camera
+        url (str): Url of the camera
         audio (bool): Whether audio is included in the stream or not
-        camera (Camera): The camera object containing
         root (str): Path to files where the camera stream should be put
+        stream_options (StreamOptions): object containing information on how to process the stream
 
     Returns:
         SubProcess: ffmpeg subprocess with the correct parameters
     """
-    # Get variables from environment
-    segment_size, segment_amount, encoding, _, _ = get_stream_variables()
 
     # Default value when audio is not included
     maps = ['-map', '0:0', '-map', '0:0', '-map', '0:0']
@@ -43,26 +41,26 @@ def get_conversion_process(ip_address, audio, camera, root):
     # Returns the opened path
     return Popen([
         # ffmpeg configurations
-        'ffmpeg', '-loglevel', 'fatal', '-rtsp_transport', 'tcp', '-i', ip_address,
+        'ffmpeg', '-loglevel', 'fatal', '-rtsp_transport', 'tcp', '-i', url,
         # Create 3 variances of video + audio stream
         *maps,
         '-profile:v', 'main', '-crf', '24', '-force_key_frames', 'expr:gte(t,n_forced*2)',
         '-sc_threshold', '0', '-g', '24', '-muxdelay', '0', '-keyint_min', '24',
         *caarg,
         # Set common properties of the video variances
-        '-s:v:0', '640x360', '-c:v:0', encoding, '-b:v:0', '800k', '-maxrate',
+        '-s:v:0', '640x360', '-c:v:0', stream_options.encoding, '-b:v:0', '800k', '-maxrate',
         '900k', '-bufsize', '1200k',  # 360p - Low bit-rate Stream
-        '-s:v:1', '854x480', '-c:v:1', encoding, '-b:v:1', '1425k', '-maxrate',
+        '-s:v:1', '854x480', '-c:v:1', stream_options.encoding, '-b:v:1', '1425k', '-maxrate',
         '1600k', '-bufsize', '2138k',  # 420p - Medium bit-rate Stream
-        '-s:v:2', '1280x720', '-c:v:2', encoding, '-b:v:2', '2850k', '-maxrate',
+        '-s:v:2', '1280x720', '-c:v:2', stream_options.encoding, '-b:v:2', '2850k', '-maxrate',
         '3200k', '-bufsize', '4275k',  # 720p - High bit-rate Stream
         *copy,  # Copy original audio to the video variances
         '-var_stream_map', stream_map,
         # Create the master playlist
-        '-master_pl_name', f'{camera}.m3u8',
+        '-master_pl_name', 'stream.m3u8',
         # HLS flags and segment properties
-        '-hls_time', segment_size, '-hls_list_size', segment_amount, '-hls_flags',
+        '-hls_time', stream_options.segment_size, '-hls_list_size', stream_options.segment_amount, '-hls_flags',
         'delete_segments', '-start_number', '1',
         # Url
-        f'{root}/{camera}_V%v.m3u8'
+        f'{root}/stream_V%v.m3u8'
     ])
