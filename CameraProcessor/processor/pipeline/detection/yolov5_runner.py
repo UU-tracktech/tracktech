9c3d0c9a883231bbab6e3a077b930d77d49c57f0
Jaimy Kwa <jaimykwa@gmail.com>
2021-05-07 17:36:46 +0200

    TST Changed it back, yolov5detector now checks dynamically
    If you are trying to use GPU but its unavailable, switches to CPU automatically
    
    
    Former-commit-id: 94b8ec711e71ba93a2772f722b2b524e2e4c890c

--- CameraProcessor/processor/pipeline/detection/yolov5_runner.py
+++ CameraProcessor/processor/pipeline/detection/yolov5_runner.py
@@ -49,6 +49,9 @@ class Yolov5Detector(IDetector):
         # Initialize
         set_logging()
         self.device = select_device(self.config['device'])
+        if not torch.cuda.is_available():
+            logging.info("CUDA unavailable")
+            self.device = 'cpu'
         self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
         if self.device.type == 'cpu':
             logging.info("I am using the CPU. Check CUDA version,"
