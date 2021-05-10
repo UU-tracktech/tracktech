ebb008678a8f693eb1f7c7c843d329012de343ba
GerardvSchie <g.j.vanschie@students.uu.nl>
2021-05-10 21:36:58 +0200

    OTH: Working tornado webpage
    Former-commit-id: e7eb9b062db5798329e3c7a7fe3c78a59c99669f

--- CameraProcessor/processor/main.py
+++ CameraProcessor/processor/main.py
@@ -11,13 +11,14 @@ import logging
 import os
 import asyncio
 import configparser
+import cv2
 from absl import app
 
 import tornado.ioloop
 import tornado.web
 
-from processor.webhosting.html_page_handler import HtmlPageHandler
-from processor.webhosting.stream_handler import StreamHandler
+import processor.utils.text as text
+import processor.utils.draw as draw
 
 from processor.pipeline.detection.yolov5_runner import Yolov5Detector
 from processor.input.video_capture import VideoCapture
@@ -28,7 +29,7 @@ from processor.pipeline.tracking.sort_tracker import SortTracker
 import processor.webhosting.websocket_client as client
 
 
-def main(_):
+def main():
     """Setup for logging and starts pipeline by reading in config information.
 
     Args:
@@ -66,7 +67,46 @@ def main(_):
     # Get orchestrator configuration
     orchestrator_config = configs['Orchestrator']
 
-    asyncio.get_event_loop().run_until_complete(initialize(vid_stream, detector, tracker, orchestrator_config['url']))
+    return vid_stream, detector, tracker, orchestrator_config['url']
+
+
+async def __send_orchestrator(ws_client, frame_obj, tracked_boxes):
+    """Sends the bounding boxes to the orchestrator
+
+    Args:
+        frame_obj (FrameObj): Frame object on which drawing takes place
+        tracked_boxes (BoundingBoxes):
+    """
+    client_message = text.bounding_boxes_to_json(tracked_boxes, frame_obj.get_timestamp())
+    ws_client.write_message(client_message)
+    logging.info(client_message)
+    await asyncio.sleep(0)
+
+
+async def __opencv_display(frame_obj, tracked_boxes):
+    """Displays frame using the imshow of opencv
+
+    Args:
+        frame_obj (FrameObj): Frame object on which drawing takes place
+        tracked_boxes (BoundingBoxes):
+    """
+    # Copy frame to draw over.
+    frame_copy = frame_obj.get_frame().copy()
+
+    # Draw bounding boxes with ID
+    draw.draw_tracking_boxes(frame_copy, tracked_boxes.get_bounding_boxes())
+
+    # Play the video in a window called "Output Video"
+    try:
+        cv2.imshow("Output Video", frame_copy)
+    except OSError as err:
+        # Figure out how to get Docker to use GUI
+        raise OSError("Error displaying video. Are you running this in Docker perhaps?") \
+            from err
+
+    # This next line is **ESSENTIAL** for the video to actually play
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        exit()
 
 
 async def initialize(vid_stream, detector, tracker, url):
@@ -78,14 +118,16 @@ async def initialize(vid_stream, detector, tracker, url):
         tracker (SortTracker): tracker performing SORT tracking.
         url (str): url to the processor orchestrator.
     """
+    func = None
     if isinstance(vid_stream, HlsCapture):
         ws_id = vid_stream.hls_url
         ws_client = await client.create_client(url, ws_id)
+        func = lambda frame_obj, bounding_boxes: __send_orchestrator(ws_client, frame_obj, bounding_boxes)
     else:
         ws_client = None
+        func = __opencv_display
 
-    await process_stream(vid_stream, detector, tracker, ws_client)
-
+    await process_stream(vid_stream, detector, tracker, func)
 
 if __name__ == '__main__':
     # Configure the logging
@@ -95,7 +137,11 @@ if __name__ == '__main__':
                         datefmt='%Y-%m-%d %H:%M:%S')
 
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+    vid_stream, detector, tracker, url = main()
+
     try:
-        app.run(main)
+        asyncio.get_event_loop().run_until_complete(
+            initialize(vid_stream, detector, tracker, url))
     except SystemExit:
         pass
