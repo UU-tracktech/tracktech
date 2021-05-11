2d244dab760770e5459e95ddea3035d1e49595c8
J.Teeuwissen <j.teeuwissen@students.uu.nl>
2021-05-11 16:46:26 +0200

    FFT: seperate environment variables for different qualities
    Former-commit-id: c14fda05a84ed88b7d5715d0780dad2d20366218

--- VideoForwarder/src/conversion_process.py
+++ VideoForwarder/src/conversion_process.py
@@ -22,60 +22,51 @@ def get_conversion_process(url, audio, root, stream_options):
         SubProcess: ffmpeg subprocess with the correct parameters
     """
 
-    map_count = sum([stream_options.low, stream_options.medium, stream_options.high])
+    index = 0
+    maps = []
+    conversions = []
+    stream_map = []
 
-    # Audio enabled
-    if audio:
-        maps = [
-            [],
-            ['-map', '0:0', '-map', '0:1'],
-            ['-map', '0:0', '-map', '0:1', '-map', '0:0', '-map', '0:1'],
-            ['-map', '0:0', '-map', '0:1', '-map', '0:0', '-map', '0:1', '-map', '0:0', '-map', '0:1']
-        ]
-        caarg = ['-c:a', 'aac', '-ar', '48000']
-        copy = ['-c:a', 'copy']
-        stream_map = [
-            '',
-            'v:0,a:0',
-            'v:0,a:0 v:1,a:1',
-            'v:0,a:0 v:1,a:1 v:2,a:2'
-        ]
-    # No Audio enabled
-    else:
-        maps = [
-            [],
-            ['-map', '0:0'],
-            ['-map', '0:0', '-map', '0:0'],
-            ['-map', '0:0', '-map', '0:0', '-map', '0:0']
-        ]
-        caarg = []
-        copy = []
-        stream_map = [
-            '',
-            'v:0',
-            'v:0 v:1',
-            'v:0 v:1 v:2'
-        ]
+    if stream_options.low:
+        maps.extend(['-map', '0:0', '-map', '0:1'] if audio else ['-map', '0:0'])
+        conversions.extend([
+            f'-s:v:{index}', '640x360', f'-c:v:{index}', stream_options.encoding,
+            f'-b:v:{index}', '800k', '-maxrate', '900k', '-bufsize', '1200k'
+        ])  # 360p - Low bit-rate Stream
+        stream_map.append(f'v:{index},a:{index}' if audio else f'v:{index}')
+        index += 1
+        
+    if stream_options.medium:
+        maps.extend(['-map', '0:0', '-map', '0:1'] if audio else ['-map', '0:0'])
+        conversions.extend([
+            f'-s:v:{index}', '854x480', f'-c:v:{index}', stream_options.encoding,
+            f'-b:v:{index}', '1425k', '-maxrate', '1600k', '-bufsize', '2138k'
+        ])  # 420p - Medium bit-rate Stream
+        stream_map.append(f'v:{index},a:{index}' if audio else f'v:{index}')
+        index += 1
+        
+    if stream_options.high:
+        maps.extend(['-map', '0:0', '-map', '0:1'] if audio else ['-map', '0:0'])
+        conversions.extend([
+            f'-s:v:{index}', '1280x720', f'-c:v:{index}', stream_options.encoding,
+            f'-b:v:{index}', '2850k', '-maxrate', '3200k', '-bufsize', '4275k'
+        ])  # 720p - High bit-rate Stream
+        stream_map.append(f'v:{index},a:{index}' if audio else f'v:{index}')
 
     # see https://developer.nvidia.com/video-encode-and-decode-gpu-support-matrix-new
     # Returns the opened path
-    return Popen([
+    command = [
         # ffmpeg configurations
         'ffmpeg', '-loglevel', 'fatal', '-rtsp_transport', 'tcp', '-i', url,
         # Create 3 variances of video + audio stream
-        *maps[map_count],
+        *maps,
         '-profile:v', 'main', '-crf', '24', '-force_key_frames', 'expr:gte(t,n_forced*2)',
         '-sc_threshold', '0', '-g', '24', '-muxdelay', '0', '-keyint_min', '24',
-        *caarg,
+        *(['-c:a', 'aac', '-ar', '48000'] if audio else []),
         # Set common properties of the video variances
-        '-s:v:0', '640x360', '-c:v:0', stream_options.encoding, '-b:v:0', '800k', '-maxrate',
-        '900k', '-bufsize', '1200k',  # 360p - Low bit-rate Stream
-        '-s:v:1', '854x480', '-c:v:1', stream_options.encoding, '-b:v:1', '1425k', '-maxrate',
-        '1600k', '-bufsize', '2138k',  # 420p - Medium bit-rate Stream
-        '-s:v:2', '1280x720', '-c:v:2', stream_options.encoding, '-b:v:2', '2850k', '-maxrate',
-        '3200k', '-bufsize', '4275k',  # 720p - High bit-rate Stream
-        *copy,  # Copy original audio to the video variances
-        '-var_stream_map', stream_map[map_count],
+        *conversions,
+        *(['-c:a', 'copy'] if audio else []),  # Copy original audio to the video variances
+        '-var_stream_map', (' '.join(stream_map)),
         # Create the master playlist
         '-master_pl_name', 'stream.m3u8',
         # HLS flags and segment properties
@@ -83,4 +74,8 @@ def get_conversion_process(url, audio, root, stream_options):
         'delete_segments', '-start_number', '1',
         # Url
         f'{root}/stream_V%v.m3u8'
-    ])
+    ]
+
+    print(command)
+
+    return Popen(command)
