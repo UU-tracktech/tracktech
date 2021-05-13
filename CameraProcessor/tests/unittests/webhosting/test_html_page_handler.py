95ee4d72311ecc2dc8abbe98e9dd0b6f8330c29e
saberzero1 <ewjbangma@hotmail.com>
2021-05-13 21:45:42 +0200

    UFT: Test html_page_handler setup
    Former-commit-id: 162431cf17c39f4c34698f6dbaa7be6218c59e6e

---
+++ CameraProcessor/tests/unittests/webhosting/test_html_page_handler.py
@@ -0,0 +1,7 @@
+"""Tests html_page_handler.py
+
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+
+"""
