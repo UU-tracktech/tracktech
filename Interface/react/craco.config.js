04e3f7a6ee9e4d1eecbe72b83e823f416f9ff77b
J.Teeuwissen <j.teeuwissen@students.uu.nl>
2021-05-14 10:33:25 +0200

    UFT: icons fixed
    Former-commit-id: 42564415950a841d7f6c421110467503a4a78c9e

--- Interface/react/craco.config.js
+++ Interface/react/craco.config.js
@@ -1,4 +1,4 @@
-const CracoLessPlugin = require('craco-less');
+const CracoLessPlugin = require('craco-less')
 
 module.exports = {
   plugins: [
@@ -7,14 +7,14 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { 
+            modifyVars: {
               '@primary-color': '#096dd9',
               '@error-color': '#cf1322'
             },
-            javascriptEnabled: true,
-          },
-        },
-      },
-    },
-  ],
-};
+            javascriptEnabled: true
+          }
+        }
+      }
+    }
+  ]
+}
