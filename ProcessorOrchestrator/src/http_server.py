51943d5a72761ec4a1fa4a6eacc3bc2227787810
Rienk <rienkfidder@gmail.com>
2021-05-17 11:52:37 +0200

    TST: Extracted some code from main.py and added some tests to improve coverage

---
+++ ProcessorOrchestrator/src/http_server.py
@@ -0,0 +1,54 @@
+"""HTTP server setup.
+
+This file contains methods to create a http or https server.
+
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+
+"""
+
+import os
+import ssl
+
+from tornado.httpserver import HTTPServer
+
+import src.logger as logger
+
+
+def create_http_servers(app):
+    """Creates http server and https server if ssl options are given in the environment
+
+    Args:
+        app(tornado.web.Application): the web application to use when creating the servers.
+
+    Returns:
+        The one or two created tornado http servers
+
+    """
+    # Get ssl ready, if provided in the environment variables
+    cert = os.environ.get('SSL_CERT')
+    key = os.environ.get('SSL_KEY')
+    use_tls = cert is not None and key is not None
+
+    https_server = None
+
+    if use_tls:
+        # Create a ssl context
+        ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        ssl_ctx.load_cert_chain(cert, key)
+
+        # Create a http server, with optional ssl.
+        https_server = HTTPServer(app, ssl_options=ssl_ctx)
+        https_server.listen(443)
+        logger.log('listening over https')
+
+    # Create a http server
+    http_server = HTTPServer(app)
+    http_server.listen(80)
+    logger.log('listening over http')
+
+    return http_server, https_server
+
+
+
