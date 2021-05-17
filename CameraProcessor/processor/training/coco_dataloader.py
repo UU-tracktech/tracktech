07fc21f5ef84218d9af87d91b08be19a772d21c7
Marieke van der Plaat <m.d.vanderplaat@uu.nl>
2021-05-17 14:27:15 +0200

    UFT split functionality of the parse file method.

--- CameraProcessor/processor/training/coco_dataloader.py
+++ CameraProcessor/processor/training/coco_dataloader.py
@@ -11,39 +11,13 @@ from processor.training.idataloader import IDataloader
 
 class COCODataloader(IDataloader):
 
+    def __init__(self, categories, file_path, nr_frames, image_path=''):
+        super().__init__(categories, file_path, nr_frames, image_path='')
+        self.coco = COCO(file_path)
+
     def parse_file(self):
-        coco = COCO(self.file_path)
-
-        # Specify a list of category names of interest
-        cat_ids = coco.getCatIds(catNms=['person'])
-        # Get the corresponding image ids and images using loadImgs
-        img_ids = coco.getImgIds(catIds=cat_ids)
-        img_ids = img_ids[:self.nr_frames]
-
-        ann_ids = coco.getAnnIds(imgIds=img_ids)
-        anns = coco.loadAnns(ann_ids)
-        with open(self.filter_config['targets_path']) as filter_names:
-            filters = filter_names.read().splitlines()
-
-        counter = 0
-        boxes = []
-        previous_name = ''
-
-        for ann in anns:
-            width, height = self.get_image_size(ann['id'])
-            box = BoundingBox(classification=coco.loadCats(ann['category_id'])[0]['name'],
-                              rectangle=Rectangle(x1=ann['bbox'][0] / width,
-                                                  y1=ann['bbox'][1] / height,
-                                                  x2=(ann['bbox'][0] + ann['bbox'][2]) / width,
-                                                  y2=(ann['bbox'][1] + ann['bbox'][3]) / height),
-                              identifier=str(counter),
-                              score=1)
-            if box.label in filters:
-                boxes.append(box)
-            current_name = ann['image_id']
-            if current_name != previous_name:
-                counter += 1
-            previous_name = current_name
+        annotations = self.__get_annotations()
+        boxes = self.__parse_boxes(annotations)
         return BoundingBoxes(boxes)
 
     def download_coco_images(self, amount):
@@ -55,19 +29,64 @@ class COCODataloader(IDataloader):
         Returns: COCO images in the image_upload_path folder.
 
         """
-        coco = COCO(self.accuracy_config['gt_path'])
-
         # Specify a list of category names of interest
-        cat_ids = coco.getCatIds(catNms=['person'])
+        cat_ids = self.coco.getCatIds(catNms=['person'])
         # Get the corresponding image ids and images using loadImgs
-        img_ids = coco.getImgIds(catIds=cat_ids)
+        img_ids = self.coco.getImgIds(catIds=cat_ids)
         img_ids = img_ids[:amount]
-        images = coco.loadImgs(img_ids)
+        images = self.coco.loadImgs(img_ids)
 
         for image in images:
             image_data = requests.get(image['coco_url']).content
-            with open(self.accuracy_config['image_upload_path'] + '/' + image['file_name'], 'wb') as handler:
+            with open(self.image_path + '/' + image['file_name'], 'wb') as handler:
                 handler.write(image_data)
+
+    def __parse_boxes(self, annotations):
+        counter = 0
+        boxes = []
+        previous_name = ''
+
+        for ann in annotations:
+            width, height = self.get_image_size(ann['id'])
+            box = BoundingBox(classification=self.coco.loadCats(ann['category_id'])[0]['name'],
+                              rectangle=Rectangle(x1=ann['bbox'][0] / width,
+                                                  y1=ann['bbox'][1] / height,
+                                                  x2=(ann['bbox'][0] + ann['bbox'][2]) / width,
+                                                  y2=(ann['bbox'][1] + ann['bbox'][3]) / height),
+                              identifier=counter,
+                              certainty=1)
+            current_name = ann['image_id']
+            if current_name != previous_name:
+                counter += 1
+            previous_name = current_name
+        return boxes
+
+    def __get_annotations(self):
+        annotations = self.__get_all_annotations()
+        annotations = self.__filter_annotations(annotations)
+        return annotations
+
+    def __get_all_annotations(self):
+        # Specify a list of category names of interest
+        cat_ids = self.coco.getCatIds(catNms=self.categories)
+        # Get the corresponding image ids and images using loadImgs
+        img_ids = self.coco.getImgIds(catIds=cat_ids)
+        img_ids = img_ids[:self.nr_frames]
+
+        ann_ids = self.coco.getAnnIds(imgIds=img_ids)
+        anns = self.coco.loadAnns(ann_ids)
+        return anns
+
+    def __filter_annotations(self, annotations):
+        filtered_annotations = []
+
+        with open(self.filter_config['targets_path']) as filter_names:
+            filters = filter_names.read().splitlines()
+
+        for ann in annotations:
+            if self.coco.loadCats(ann['category_id'])[0]['name'] in filters:
+                filtered_annotations.append(ann)
+        return filtered_annotations
 
     def get_image_size(self, image_id):
         """Gets the size of an image based on its COCO name.
--- CameraProcessor/processor/training/idataloader.py
+++ CameraProcessor/processor/training/idataloader.py
@@ -1,11 +1,19 @@
+from processor.utils.config_parser import ConfigParser
+
+
 class IDataloader:
-    def __init__(self, file_path, nr_frames, image_path=''):
+    def __init__(self, categories, file_path, nr_frames, image_path=''):
         self.file_path = file_path
         self.image_path = image_path
+        self.categories = categories
         # Cannot contain negative amount of frames.
         if nr_frames < 0:
             raise AttributeError('Cannot have negative number of frames')
         self.nr_frames = nr_frames
+
+        config_parser = ConfigParser('configs.ini')
+        configs = config_parser.configs
+        self.filter_config = configs['Filter']
 
     def get_image_size(self, image_id):
         """Gets the size of an image based on its name.
