53177e21a4c217212d6d68c9f87df112016ec3be
Jaimy Kwa <jaimykwa@gmail.com>
2021-05-19 15:22:16 +0200

    FIX Proper LFS, symlinked yolor

--- CameraProcessor/tests/unittests/pipeline/test_process_frames.py
+++ CameraProcessor/tests/unittests/pipeline/test_process_frames.py
@@ -13,6 +13,7 @@ from processor.pipeline.process_frames import send_to_orchestrator
 from processor.utils.config_parser import ConfigParser
 from processor.pipeline.process_frames import process_stream, prepare_stream
 from processor.pipeline.detection.yolov5_runner import Yolov5Detector
+from processor.pipeline.detection.yolor_runner import YolorDetector
 from processor.input.video_capture import VideoCapture
 
 from tests.unittests.utils.fake_detector import FakeDetector
@@ -51,6 +52,15 @@ class TestProcessFrames:
         return Yolov5Detector(configs['Yolov5'], configs['Filter'])
 
     # pylint: disable=useless-return
+    def __get_yolorrunner(self):
+        """Get the YOLOR runner
+
+        """
+        config_parser = ConfigParser('configs.ini')
+        configs = config_parser.configs
+        return YolorDetector(configs['Yolor'], configs['Filter'])
+
+    # pylint: disable=useless-return
     def __get_sort_tracker(self):
         """Get the SORT tracker.
         """
@@ -68,6 +78,23 @@ class TestProcessFrames:
         capture = self.__get_video()
         unused_capture, detector, tracker, _ = prepare_stream()
         unused_capture.close()
+
+        asyncio.get_event_loop().run_until_complete(self.await_detection(capture, detector, tracker))
+        capture.close()
+
+    @pytest.mark.timeout(90)
+    def test_process_stream_with_yolor(self):
+        """Tests process_stream function with YOLOR
+
+        Note: Not parametrizing Yolor for the same reason as previous function, but this is a bunch of duplicate
+        code now so that sucks.
+
+        """
+        # Open video
+        capture = self.__get_video()
+        unused_capture, _, tracker, _ = prepare_stream()
+        unused_capture.close()
+        detector = self.__get_yolorrunner()
 
         asyncio.get_event_loop().run_until_complete(self.await_detection(capture, detector, tracker))
         capture.close()
