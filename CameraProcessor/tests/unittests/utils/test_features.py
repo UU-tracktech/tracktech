6bf22759bcfcf9d8b993d920ba5405ffda2997c3
Jarelk <j.t.s.kwa@students.uu.nl>
2021-05-19 13:44:46 +0200

    Spc 560 make cutout of boundingbox (#17)
    * FFT Added a method to slice bounding box
    
    
    Former-commit-id: 0e1f688f434cee835ac378a8e09ce9c37b571e01
    
    * DOC Documented how the memory allocation works for slice
    
    
    Former-commit-id: 76b41340a0fd2b94002aa0045c5e4a8511d0bb65
    
    * DOC added TODO for the test
    
    
    Former-commit-id: 91325fa702da121518cb799c4f308c85e715ad81
    
    * FFT Added both cutout and resizing
    
    * FIX linting
    
    * FIX Linting
    
    * CRF Renaming utils file
    
    * FIX Fixed... something?
    
    * FIX typo
    
    Co-authored-by: Jaimy Kwa <jaimykwa@gmail.com>

---
+++ CameraProcessor/tests/unittests/utils/test_features.py
@@ -0,0 +1,49 @@
+"""Tests the reid utils
+
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+
+"""
+
+import cv2
+import numpy as np
+from processor.utils.features import slice_bounding_box, resize_cutout
+from processor.utils.config_parser import ConfigParser
+from tests.unittests.utils.conftest import X0, X1, Y0, Y1
+
+
+class TestReidUtils:
+    """Class to hold functions to test reid utils
+
+    """
+
+    def test_slice_bounding_box(self, img, bbox):
+        """Tests the slice_bounding_box method
+
+        """
+        # red color to fill the bbox
+        color = np.array([0, 0, 255])
+
+        # Color the bounding box rectangle in the image
+        cv2.rectangle(img,
+                      (X0, Y0),
+                      (X1, Y1),
+                      (0, 0, 255),
+                      -1)
+        # Slice the image
+        sliced = slice_bounding_box(bbox, img)
+
+        # Assert the sliced image is now entirely red
+        width, height, _ = sliced.shape
+        for i in range(0, width):
+            for j in range(0, height):
+                assert (sliced[i, j] == color).all()
+
+    def test_resize(self, img):
+        """Tests the resize function
+
+        """
+        config = ConfigParser('configs.ini')
+        size = config.configs['Reid'].gettuple('size')
+        assert size[0], size[1] == resize_cutout(img).shape
