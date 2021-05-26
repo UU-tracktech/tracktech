d9678147c05fb0fb58402ebcf6627e78111e567e
Marieke van der Plaat <m.d.vanderplaat@uu.nl>
2021-05-26 16:18:25 +0200

    FIX Confidence is a float instead of a tensor

--- CameraProcessor/processor/pipeline/detection/idetector.py
+++ CameraProcessor/processor/pipeline/detection/idetector.py
@@ -98,7 +98,7 @@ class IDetector(IComponent):
                         Rectangle(int(xyxy[0]) / width, int(xyxy[1]) / height, int(xyxy[2]) / width,
                                   int(xyxy[3]) / height),
                         names[int(cls)],
-                        conf
+                        conf.item()
                     )
                     if any(x == bbox.get_classification() for x in filter_types):
                         bounding_boxes.append(bbox)
