cce81305b80771f2042af0a81820187a95d1e598
GerardvSchie <g.j.vanschie@students.uu.nl>
2021-06-04 13:26:10 +0200

    OTH: Put changes on different branch

---
+++ ProcessorOrchestrator/src/__init__.py
@@ -0,0 +1,6 @@
+"""Init.
+
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+"""
