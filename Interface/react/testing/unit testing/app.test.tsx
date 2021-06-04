a958cc8c970ce21e483b4716b2d72555e580f087
Brian <brianvanbeusekom@hotmail.com>
2021-06-04 11:30:54 +0200

    Spc 788 reuse mocks on interface (#100)
    * UFT one mock to rule them all
    
    * FFT: Single keycloak mock
    
    * TSTL updated app to new KC mock
    
    * TST authState updated to new KC mock
    
    * DOC consistency
    
    * DOC more updates for consistency
    
    * TST updated loggedInUser to new KC mock
    
    * TST updated loginButton to new KC mock
    
    * DOC test comment consistency
    
    * DOC again more consistency in comments
    
    * TST fix integration and updated dockerfile

--- Interface/react/testing/unit testing/app.test.tsx
+++ Interface/react/testing/unit testing/app.test.tsx
@@ -7,99 +7,90 @@ Utrecht University within the Software Project course.
  */
 
 /*
-
-This test checks if app renders the needLogin page depending on 
-user authentication
-
+  This test checks if app renders the needLogin page depending on 
+  user authentication
 */
 
 import React from 'react'
 import { act, getByRole, render, screen } from '@testing-library/react'
 import { App } from '../../src/app'
 
-//Mock values for the keycloak mock
-let mockInitialized = true
-let mockAuthenticated = false
-let mockLogin = jest.fn()
-
-//Keycloak mock
-jest.mock('@react-keycloak/web', () => {
-  return {
-    useKeycloak: () => ({
-      initialized: mockInitialized,
-      keycloak: {
-        authenticated: mockAuthenticated,
-        login: mockLogin,
-        tokenParsed: { name: 'Firstname Lastname' }
-      }
-    })
-  }
-})
-
-/** Test to simply check if the page renders */
 test('App renders without errors', async () => {
   await act(async () => {
     render(<App />)
   })
 })
 
-/** Test if the not logged in notification shows up when not authenticated */
-test('shows login notification if not authenticated', async () => {
-  jest.setTimeout(30000)
-  mockInitialized = true
-
-  await act(async () => {
-    render(<App />)
-  })
-
-  while (screen.queryByTestId('loginAlert') == null) {
-    await new Promise((r) => setTimeout(r, 500))
-  }
-
-  expect(screen.queryByTestId('loginAlert')).toBeDefined()
-})
-
-/** Test if closing the notification calls the close function which should call login */
-test('redirects to login when closing notification', async () => {
-  jest.setTimeout(30000)
-  mockInitialized = true
-
-  await act(async () => {
-    render(<App />)
-  })
-
-  while (screen.queryByTestId('loginAlert') == null) {
-    await new Promise((r) => setTimeout(r, 500))
-  }
-
-  const alert = screen.getByTestId('loginAlert')
-  const btn = getByRole(alert, 'button')
-
-  expect(btn).toBeDefined()
-
-  btn.click()
-  expect(mockLogin).toBeCalled()
-})
-
-/** Test to check if the page shows home when the user is authenticated */
-test('shows home if authenticated', async () => {
-  mockInitialized = true
-  mockAuthenticated = true
-
-  await act(async () => {
-    render(<App />)
-  })
-
-  expect(screen.queryByTestId('loginAlert')).toBeFalsy()
-})
-
+//Start with testing what happens when keycloak has not finished loading yet
 test('waits for keycloak to load before showing any contents', async () => {
-  mockInitialized = false
+  require('@react-keycloak/web').__SetMockInitialized(false)
 
   await act(async () => {
     render(<App />)
   })
 
   expect(screen.queryByTestId('emptyWaitDiv')).toBeDefined()
+  expect(screen.queryByTestId('loginAlert')).toBeFalsy()
+})
+
+//Once keycloak is loaded and a user is not logged in it should show a login alert
+test('shows login notification if not authenticated', async () => {
+  jest.setTimeout(30000)
+
+  //Configure the keycloak mock
+  require('@react-keycloak/web').__SetMockInitialized(true)
+  require('@react-keycloak/web').__SetMockAuthenticated(false)
+
+  await act(async () => {
+    render(<App />)
+  })
+
+  //Look for the login alert
+  while (screen.queryByTestId('loginAlert') == null) {
+    await new Promise((r) => setTimeout(r, 500))
+  }
+  expect(screen.queryByTestId('loginAlert')).toBeDefined()
+})
+
+//Closing the alert should call the login function
+test('redirects to login when closing notification', async () => {
+  jest.setTimeout(30000)
+
+  //good practice to not rely on mock values still being there from a previous test
+  //in case of a crash for example. Redo all values needed for the test
+  require('@react-keycloak/web').__SetMockInitialized(true)
+  require('@react-keycloak/web').__SetMockAuthenticated(false)
+  //Also add a login function mock that we can check
+  let mockLogin = jest.fn()
+  require('@react-keycloak/web').__SetMockLoginFunction(mockLogin)
+
+  await act(async () => {
+    render(<App />)
+  })
+
+  //Get the alert, find the close button and close it
+  while (screen.queryByTestId('loginAlert') == null) {
+    await new Promise((r) => setTimeout(r, 500))
+  }
+  const alert = screen.getByTestId('loginAlert')
+  const btn = getByRole(alert, 'button')
+  expect(btn).toBeDefined()
+  btn.click()
+
+  //after clicking expect the login function to be called
+  expect(mockLogin).toBeCalled()
+})
+
+//Test to check if the page shows home when the user is authenticated
+test('shows home if authenticated', async () => {
+  //Again, don't rely on values still being there, reset all required onces
+  require('@react-keycloak/web').__SetMockInitialized(true)
+  require('@react-keycloak/web').__SetMockAuthenticated(true)
+
+  await act(async () => {
+    render(<App />)
+  })
+
+  //Since we're logged in we expect the alert to not be there
   expect(screen.queryByTestId('loginAlert')).toBeFalsy()
 })
