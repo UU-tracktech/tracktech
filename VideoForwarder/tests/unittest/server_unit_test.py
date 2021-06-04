a346e766ccdf57d7a66a7ec7acdbffa9e2da02b6
J.Teeuwissen <j.teeuwissen@students.uu.nl>
2021-06-04 16:47:55 +0200

    FIX: audio removed

--- VideoForwarder/tests/unittest/server_unit_test.py
+++ VideoForwarder/tests/unittest/server_unit_test.py
@@ -20,7 +20,7 @@ class TestHandler(AsyncHTTPTestCase):
     def get_app(self):
         """Creates the application to test."""
         environ["CAMERA_URL"] = "rtmp://localhost/stream"
-        environ["CAMERA_AUDIO"] = "true"
+        environ["CAMERA_AUDIO"] = "false"
 
         environ["STREAM_LOW"] = 'true'
         environ["STREAM_MEDIUM"] = 'true'
