d2c9b6bb4a594366f27fb7837d2f61bfaf5c3bcc
saberzero1 <ewjbangma@hotmail.com>
2021-06-05 12:26:44 +0200

    DOC: Added copyright docstring

--- CameraProcessor/tests/unittests/dataloaders/__init__.py
+++ CameraProcessor/tests/unittests/dataloaders/__init__.py
@@ -3,5 +3,4 @@
 This program has been developed by students from the bachelor Computer Science at
 Utrecht University within the Software Project course.
 © Copyright Utrecht University (Department of Information and Computing Sciences)
-
 """
