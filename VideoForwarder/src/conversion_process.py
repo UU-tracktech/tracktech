676eb8df97141d1fe80a1280fd98153db69e9d5a
Li-Yeun <45470843+Li-Yeun@users.noreply.github.com>
2021-06-05 22:30:54 +0200

    FFT: fixed stretched video footages

--- VideoForwarder/src/conversion_process.py
+++ VideoForwarder/src/conversion_process.py
@@ -28,9 +28,9 @@ def get_conversion_process(url, audio, root, stream_options):
 
     # Options for the low stream quality.
     if stream_options.low:
-        maps.extend(["-map", "0:0", "-map", "0:1"] if audio else ["-map", "0:0"])
+        maps.extend(['-map', '0:0', '-map', '0:1'] if audio else ['-map', '0:0'])
         conversions.extend([
-            f"-s:v:{index}", "640x360", f"-c:v:{index}", stream_options.encoding,
+            f'-vf:v:{index}', 'scale=640:-2', f'-c:v:{index}', stream_options.encoding,
             f'-b:v:{index}', '800k', '-maxrate', '900k', '-bufsize', '1200k'
         ])  # 360p - Low bit-rate Stream
         stream_map.append(f'v:{index},a:{index}' if audio else f'v:{index}')
@@ -40,7 +40,7 @@ def get_conversion_process(url, audio, root, stream_options):
     if stream_options.medium:
         maps.extend(['-map', '0:0', '-map', '0:1'] if audio else ['-map', '0:0'])
         conversions.extend([
-            f'-s:v:{index}', '854x480', f'-c:v:{index}', stream_options.encoding,
+            f'-vf:v:{index}', 'scale=854:-2', f'-c:v:{index}', stream_options.encoding,
             f'-b:v:{index}', '1425k', '-maxrate', '1600k', '-bufsize', '2138k'
         ])  # 420p - Medium bit-rate Stream
         stream_map.append(f'v:{index},a:{index}' if audio else f'v:{index}')
@@ -50,7 +50,7 @@ def get_conversion_process(url, audio, root, stream_options):
     if stream_options.high:
         maps.extend(['-map', '0:0', '-map', '0:1'] if audio else ['-map', '0:0'])
         conversions.extend([
-            f'-s:v:{index}', '1280x720', f'-c:v:{index}', stream_options.encoding,
+            f'-vf:v:{index}', 'scale=1280:-2', f'-c:v:{index}', stream_options.encoding,
             f'-b:v:{index}', '2850k', '-maxrate', '3200k', '-bufsize', '4275k'
         ])  # 720p - High bit-rate Stream
         stream_map.append(f'v:{index},a:{index}' if audio else f'v:{index}')
@@ -59,7 +59,7 @@ def get_conversion_process(url, audio, root, stream_options):
     # See https://developer.nvidia.com/video-encode-and-decode-gpu-support-matrix-new.
     command = [
         # Ffmpeg configurations.
-        'ffmpeg', '-loglevel', 'fatal', '-i', url,
+        'ffmpeg', '-loglevel', 'fatal', '-re', '-i', url,
         # Create 3 variances of video + audio stream.
         *maps,
         '-profile:v', 'main', '-crf', '24', '-force_key_frames', 'expr:gte(t,n_forced*2)',
