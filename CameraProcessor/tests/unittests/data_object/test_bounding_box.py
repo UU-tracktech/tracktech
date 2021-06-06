ace5639a6424cb6c71d8117d507d70cee45e431a
saberzero1 <ewjbangma@hotmail.com>
2021-06-06 14:48:24 +0200

    CRF: Test FrameObj

--- CameraProcessor/tests/unittests/data_object/test_bounding_box.py
+++ CameraProcessor/tests/unittests/data_object/test_bounding_box.py
@@ -22,7 +22,7 @@ class TestBoundingBox:
     """
 
     def setup_method(self):
-        """Set ups bounding_box for unit testing."""
+        """Sets up bounding_box for unit testing."""
         self.box1 = BoundingBox(1, Rectangle(0, 0.5, 0.75, 1), "person", 0.5, object_id=5)
         self.box1_duplicate = BoundingBox(1, Rectangle(0, 0.5, 0.75, 1), "person", 0.5, object_id=5)
         self.box2 = BoundingBox(1, Rectangle(0.1, 0.25, 0.5, 0.9), "car", 0.75, object_id=15)
@@ -49,7 +49,6 @@ class TestBoundingBox:
             'rectangle: Rectangle(x1:0.000 y1:0.500 x2:0.750 y2:1.000))'
         assert s == self.box1.__repr__()
 
-    # Testing form.
     def test_range_certainty(self):
         """Asserts if certainty is within range 0...1."""
         assert self.certainty <= 1
