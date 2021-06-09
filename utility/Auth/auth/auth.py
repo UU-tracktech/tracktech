f51622d750d12b147d42d15d636cc6dd193fc3e4
Jaimy Kwa <jaimykwa@gmail.com>
2021-06-09 13:52:03 +0200

    FIX Hopefully added auth linting

--- utility/Auth/auth/auth.py
+++ utility/Auth/auth/auth.py
@@ -1,73 +1,69 @@
+# pylint: disable=multiple-class-definitions.
 """Handles authentication and authorization of tokens.
 
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
 """
 
 import jwt
 
 
 class Auth:
-    """Keeps track of authentication options in order to validate tokens.
-
-    """
+    """Keeps track of authentication options in order to validate tokens. """
     def __init__(self, public_key_path, algorithms, audience, role):
-        """Constructor
+        """Constructor.
+
         Args:
-            public_key_path: the path to the public key file
-            algorithms: the algorithms to use to validate the token
-            audience: the audience for the token
-            role: the role the user needs
+            public_key_path (str): the path to the public key file
+            algorithms (list): the algorithms to use to validate the token
+            audience (iterable): the audience for the token
+            role (str): the role the user needs
         """
 
-        # Read the key file
+        # Read the key file.
         public_key_file = open(public_key_path, "r")
         self.public_key = public_key_file.read()
         public_key_file.close()
 
-        # Save the other arguments
+        # Save the other arguments.
         self.algorithms = algorithms
         self.audience = audience
         self.role = role
 
     def validate(self, token) -> None:
-        """Validate the given token
-        
+        """Validate the given token.
+
         Args:
-            token: an jwt token to validate
+            token (str): an jwt token to validate
         """
 
         try:
-            # Decode the token using the given key and the header token
+            # Decode the token using the given key and the header token.
             decoded = jwt.decode(token, self.public_key, algorithms=self.algorithms, audience=self.audience)
 
         except Exception as e:
-            # If decoding fails throw a AuthenticationError
-            raise AuthenticationError("Failed to authenticate", e)
-        
+            # If decoding fails throw a AuthenticationError.
+            raise AuthenticationError("Failed to authorize", e) from e
+
         try:
             authorized = self.role in decoded["resource_access"][self.audience]["roles"]
-                # If decoding succeeds but the role is invalid throw a AuthorizationError
-                
+            # If decoding succeeds but the role is invalid throw a AuthorizationError.
+
         except Exception as e:
-            # If decoding fails throw a AuthenticationError
-            raise AuthenticationError("Failed to authorize", e)
-        
+            # If decoding fails throw a AuthenticationError.
+            raise AuthenticationError("Failed to authorize", e) from e
+
         if not authorized:
-                raise AuthorizationError("Role not found")
-        
+            raise AuthorizationError("Role not found")
+
         print("Authed")
 
 
 class AuthenticationError(Exception):
-    """Exception to throw when authentication fails.
-
-    """
-
-    pass
+    """Exception to throw when authentication fails."""
 
 
 class AuthorizationError(Exception):
-    """Exception to throw when authorization fails.
-
-    """
-
-    pass
+    """Exception to throw when authorization fails."""
+# pylint: enable=multiple-class-definitions.
