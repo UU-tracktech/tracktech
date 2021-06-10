4bf74aaa2212a5d7173aaec61bb35462a8c782ba
saberzero1 <ewjbangma@hotmail.com>
2021-06-11 00:49:50 +0200

    TST: Fixed tests based on feedback.

--- CameraProcessor/tests/unittests/utils/test_convert.py
+++ CameraProcessor/tests/unittests/utils/test_convert.py
@@ -9,6 +9,7 @@ import pytest
 from tests.unittests.utils.utils import get_sample_frame
 from processor.data_object.frame_obj import FrameObj
 from processor.data_object.bounding_box import BoundingBox
+from processor.data_object.bounding_boxes import BoundingBoxes
 from processor.data_object.rectangle import Rectangle
 from processor.utils.convert import to_buffer_dict
 
@@ -19,15 +20,27 @@ class TestConvert:
 
     def setup_method(self):
         """Setup method."""
-        self.frame = FrameObj(get_sample_frame(), 12)
+        self.frame_obj = FrameObj(get_sample_frame(), 12)
         self.box1 = [BoundingBox(1, Rectangle(0, 0, 1, 1), "person", 0.5)]
-        self.dict1 = to_buffer_dict(self.frame, self.box1)
+        self.dict1 = to_buffer_dict(self.frame_obj, self.box1)
 
     def test_convert(self):
         """Tests the to_buffer_dict function."""
-        test_dict = to_buffer_dict(FrameObj(get_sample_frame(), 12),
-                                   [BoundingBox(1, Rectangle(0, 0, 1, 1), "person", 0.5)])
-        assert str(test_dict).startswith(str(self.dict1))
+        expected_dict = {
+            "frame": self.frame_obj.frame,
+            "frameId": self.frame_obj.timestamp,
+            "boxes": [
+                {
+                    "boxId": bounding_box.identifier,
+                    "rect": bounding_box.rectangle
+                }
+                for bounding_box in BoundingBoxes(self.box1)
+            ]
+        }
+
+        assert expected_dict == self.dict1
+        assert self.dict1['frameId'] == 12
+        assert len(self.dict1['frame']) > 0
 
 
 if __name__ == '__main__':
