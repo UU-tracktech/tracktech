993be7dfb05dd74ee4264e1a3dc975d0d34fb69d
J.Teeuwissen <j.teeuwissen@students.uu.nl>
2021-06-11 12:55:45 +0200

    FIX: if no longer needed

--- ProcessorOrchestrator/src/handlers/processor_socket.py
+++ ProcessorOrchestrator/src/handlers/processor_socket.py
@@ -145,10 +145,9 @@ class ProcessorSocket(WebSocketHandler):
     def on_close(self):
         """Called when the websocket is closed, deletes itself from the dict of processors."""
         logger.log_disconnect("/processor", self.request.remote_ip)
-        if self.identifier in processors:
-            # Pop the identifier instead of del as it might be called twice.
-            processors.pop(self.identifier, None)
-            logger.log(f"Processor with id {self.identifier} disconnected")
+        # Pop the identifier instead of del as it might be called twice.
+        processors.pop(self.identifier, None)
+        logger.log(f"Processor with id {self.identifier} disconnected")
 
     def data_received(self, chunk):
         """Unused method that could handle streamed request data.
