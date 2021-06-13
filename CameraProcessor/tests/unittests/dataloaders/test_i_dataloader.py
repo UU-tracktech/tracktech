aba1f573e5555484a565d5a2ccb0667e9f9e7453
Marieke van der Plaat <m.d.vanderplaat@uu.nl>
2021-06-13 14:02:37 +0200

    TST start testing idataloader

--- CameraProcessor/tests/unittests/dataloaders/test_i_dataloader.py
+++ CameraProcessor/tests/unittests/dataloaders/test_i_dataloader.py
@@ -22,17 +22,24 @@ class TestIDataloader:
     def setup_method(self):
         """Setup method."""
         configs = get_test_configs()
-        self.dataloader = IDataloader(configs, 'det_path')
+        self.dataloader = IDataloader(configs)
 
     def test_parse_file(self):
         """Tests parsing of file."""
         assert pytest.raises(NotImplementedError, self.dataloader.parse_file)
 
     def test_get_image_dimensions(self):
-        """Tests the image dimensions."""
-        image_path = os.path.join(self.dataloader.image_path, '000001.jpg')
-        dimensions = self.dataloader.get_image_dimensions(1, image_path)
-        assert dimensions == (640, 480)
+        assert pytest.raises(NotImplementedError, self.dataloader.get_image_dimensions(1))
+
+    def test_parse_boxes(self, image_id=1):
+        with pytest.raises(NotImplementedError):
+            self.dataloader.parse_boxes([(None, None, None, None, None, None, None, None, None)])
+
+    # def test_get_image_dimensions(self):
+    #     """Tests the image dimensions."""
+    #     image_path = os.path.join(self.dataloader.image_path, '000001.jpg')
+    #     dimensions = self.dataloader.get_image_dimensions(1, image_path)
+    #     assert dimensions == (640, 480)
 
     def test_get_image_dimensions_twice(self):
         """Tests the return from the dictionary."""
@@ -50,12 +57,17 @@ class TestIDataloader:
         accuracy_config = configs['Accuracy']
 
         # Check whether properties are set correctly.
-        assert self.dataloader.file_path == accuracy_config['det_path']
-        assert self.dataloader.image_path == accuracy_config['image_path']
+        # assert self.dataloader.file_path == accuracy_config['det_path']
+        # assert self.dataloader.image_path == accuracy_config['image_path']
         assert self.dataloader.categories == accuracy_config['categories']
         assert self.dataloader.filter_config == configs['Filter']
-        assert len(self.dataloader.image_dimensions) == 0
         assert self.dataloader.nr_frames == int(accuracy_config['nr_frames'])
+
+    def test_init_attribute_error(self):
+        configs = get_test_configs()
+        configs['Accuracy']['nr_frames'] = '-1'
+        with pytest.raises(AttributeError):
+            IDataloader(configs)
 
     def test_invalid_init(self):
         """Negative number of frames should raise an exception."""
