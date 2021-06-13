33cb8eb1e876272bfd6af8431eb5fe92af5f83e7
RienkF <45170310+RienkF@users.noreply.github.com>
2021-06-13 11:47:41 +0200

    FFT: Merged settings and provided acces with environment (#147)
    * FFT: Merged settings and provided acces with environment
    
    * OTH: Strange files

---
+++ Interface/react/src/components/environmentContext.tsx
@@ -0,0 +1,59 @@
+/*
+
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+
+ */
+
+import React, { ReactNode, useState } from 'react'
+
+/** Type for one settings description of a camera */
+export type cameraSettingsType = {
+  Name: string
+  Id: string
+  Forwarder: string
+}
+
+/** Type containing all the arguments needed to create a context with a websocket */
+export type environmentArgs = {
+  cameras: cameraSettingsType[]
+  objectTypes: string[]
+  orchestratorUrl: string
+}
+
+/** The context which can be used by other components get settings from the environment settings file */
+export const environmentContext = React.createContext<environmentArgs>({
+  cameras: [],
+  objectTypes: [],
+  orchestratorUrl: 'wss://echo.websocket.org' // Dummy websocket since the url must be valid
+})
+
+/** Context provider that reads settings file and serves results */
+export function EnvironmentProvider(props: { children: ReactNode }) {
+  const [environment, setEnvironment] = useState<environmentArgs>()
+
+  // Get settings and save them
+  React.useEffect(() => {
+    try {
+      fetch(process.env.PUBLIC_URL + '/settings.json').then((text) =>
+        text.json().then((json) => {
+          setEnvironment(json)
+        })
+      )
+      // Catch fail as the settings file might be invalid
+    } catch {}
+  }, [])
+
+  return (
+    <environmentContext.Provider
+      value={{
+        cameras: environment?.cameras ?? [],
+        objectTypes: environment?.objectTypes ?? [],
+        orchestratorUrl: environment?.orchestratorUrl ?? ''
+      }}
+    >
+      {props.children}
+    </environmentContext.Provider>
+  )
+}
