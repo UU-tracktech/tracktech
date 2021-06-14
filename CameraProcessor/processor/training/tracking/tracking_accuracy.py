fdb86820068a83c6d6c0e378d958a73557eab22a
bacdekker <b.a.c.dekker@students.uu.nl>
2021-06-14 11:22:54 +0200

    UFT added repo

---
+++ CameraProcessor/processor/training/tracking/tracking_accuracy.py
@@ -0,0 +1,14 @@
+"""File containing a runner to test the accuracy of a tracker provided the tracks are written in the MOT format.
+
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+"""
+
+import processor.training.tracking
+
+
+class TrackingAccuracy:
+    """ A class for the accuracy tracking runner.
+
+    """
