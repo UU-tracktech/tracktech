fe1e157e768cdfb65d9659e7301435fa0cdef2d3
bacdekker <b.a.c.dekker@students.uu.nl>
2021-06-14 16:48:30 +0200

    UFT at least it is working I suppose

---
+++ CameraProcessor/processor/training/tracking/trackeval_master/trackeval/__init__.py
@@ -0,0 +1,5 @@
+from processor.training.tracking.trackeval_master.trackeval.eval import Evaluator
+from processor.training.tracking.trackeval_master.trackeval import datasets
+from processor.training.tracking.trackeval_master.trackeval import metrics
+from processor.training.tracking.trackeval_master.trackeval import plotting
+from processor.training.tracking.trackeval_master.trackeval import utils
--- CameraProcessor/processor/training/tracking/tracking_accuracy.py
+++ CameraProcessor/processor/training/tracking/tracking_accuracy.py
@@ -5,10 +5,34 @@ Utrecht University within the Software Project course.
 © Copyright Utrecht University (Department of Information and Computing Sciences)
 """
 
-import processor.training.tracking
+from processor.utils.config_parser import ConfigParser
+import sys
+import os
+import argparse
+from subprocess import call
 
 
-class TrackingAccuracy:
-    """ A class for the accuracy tracking runner.
+def main(configs):
+    """Method for running the tracking accuracy
 
     """
+    folder_prefix = "../../../"
+    accuracy_section = configs['Accuracy']
+    det_folder = os.path.realpath(os.path.join(folder_prefix, accuracy_section['det_folder']))
+    gt_folder = os.path.realpath(os.path.join(folder_prefix, accuracy_section['gt_folder']))
+    benchmark = 'MOT20'
+    metric = 'CLEAR'
+    parallel = 'False'
+    parser_config = {"--METRICS": metric,
+                     "--USE_PARALLEL": parallel,
+                     "--BENCHMARK": benchmark,
+                     "--GT_FOLDER": gt_folder,
+                     "--TRACKERS_FOLDER": det_folder}
+    script_path = os.path.realpath('../../../processor/training/tracking/trackeval_master/scripts/run_mot_challenge.py')
+    cmd = f"""python {script_path} --USE_PARALLEL {parallel} --METRICS {metric} --BENCHMARK {benchmark} --GT_FOLDER {gt_folder} --TRACKERS_FOLDER {det_folder}"""
+    os.system(cmd)
+
+
+if __name__ == '__main__':
+    config_parser = ConfigParser('configs.ini', True)
+    main(config_parser.configs)
