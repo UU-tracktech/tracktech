197b5cf3cc6214f02e4bae2646f2ce4d64779b3c
Jarelk <j.t.s.kwa@students.uu.nl>
2021-06-16 11:54:11 +0200

    Spc 729 cosine distance vs euclidian distance (#154)
    * FFT added config option
    
    * FFT Added proper similarity checks
    
    * FIX Pylint fix, also removed merging artefact that printed in comment_checker.py
    
    * FIX Moved duplicate similarity func to parent class
    
    * FIX Linting and unit tests
    
    * FIX Pylint duplicate code
    
    * FIX Pylint duplicates in the reid runners
    
    * FIX
    
    * FIX Test
    
    * CRF Added new identifier superclass
    
    * FIX linting
    
    * FIX fake reidentifier
    
    * FIX extract_features_from_cutout now in superclass
    
    * FIX moved some not implemented functions to interface
    
    * FIX no-member
    
    * FIX missing docstring
    
    Co-authored-by: Jaimy Kwa <jaimykwa@gmail.com>

--- CameraProcessor/processor/pipeline/reidentification/fast_re_identifier.py
+++ CameraProcessor/processor/pipeline/reidentification/fast_re_identifier.py
@@ -6,19 +6,15 @@ Utrecht University within the Software Project course.
 """
 import os
 import argparse
-import copy
 import gdown
-from scipy.spatial.distance import cosine
 
-from processor.data_object.bounding_box import BoundingBox
-from processor.data_object.bounding_boxes import BoundingBoxes
 from processor.pipeline.reidentification.fastreid.fastreid.config import get_cfg
 from processor.pipeline.reidentification.fastreid.demo.predictor import FeatureExtractionDemo
-from processor.pipeline.reidentification.i_re_identifier import IReIdentifier
+from processor.pipeline.reidentification.pytorch_re_identifier import PytorchReIdentifier
 import processor.utils.features as UtilsFeatures
 
 
-class FastReIdentifier(IReIdentifier):
+class FastReIdentifier(PytorchReIdentifier):
     """Re-id class that uses fast-reid to extract and compare features.
 
     Attributes:
@@ -52,11 +48,10 @@ class FastReIdentifier(IReIdentifier):
             output = cfg.MODEL.WEIGHTS
             gdown.download(url, output, quiet=False)
 
-        self.extractor = FeatureExtractionDemo(cfg, parallel=args.parallel)
+        extractor = FeatureExtractionDemo(cfg, parallel=args.parallel)
 
-        self.config = config
-        self.threshold = float(self.config["threshold"])
         self.__feature_map_size = 2048
+        super().__init__(config, extractor)
 
     @property
     def feature_map_size(self):
@@ -66,6 +61,7 @@ class FastReIdentifier(IReIdentifier):
             int: size of the feature map.
         """
         return self.__feature_map_size
+
 
     def extract_features(self, frame_obj, bbox):
         """Extracts features from a single bounding box.
@@ -88,91 +84,3 @@ class FastReIdentifier(IReIdentifier):
         feature = self.extractor.run_on_image(resized_cutout).cpu().numpy().tolist()[0]
 
         return feature
-
-    def extract_features_boxes(self, frame_obj, boxes):
-        """Extract features from all bounding boxes generated in the tracking stage.
-
-        Args:
-            frame_obj (FrameObj): frame object storing OpenCV frame and timestamp.
-            boxes (BoundingBoxes): BoundingBoxes object that has the bounding boxes of the tracking stage.
-
-        Returns:
-            [[float]]: Feature vectors of the tracked objects.
-        """
-        features = []
-
-        for box in boxes:
-            features.append(self.extract_features(frame_obj, box))
-
-        return features
-
-    def similarity(self, query_features, gallery_features):
-        """Calculates the similarity rate between two feature vectors.
-
-        Note:
-            Uses cosine similarity to determine the similarity rate.
-            An alternative would be euclidean distance.
-
-        Args:
-            query_features ([float]): the feature vector of the query image.
-            gallery_features ([float]): the feature vector of the gallery image.
-
-        Returns:
-            float: The similarity value of two feature vectors.
-        """
-        cosine_similarity = 1 - cosine(query_features, gallery_features)
-        return cosine_similarity
-
-    def re_identify(self, frame_obj, track_obj, re_id_data):
-        """Performing re-identification using Torchreid.
-
-        This re-identification implementations couple bounding boxes to a tracked subject,
-        which is not currently detected on the camera. Updates list of bounding box by possibly assigning an object ID
-        to an existing bounding box.
-
-        Args:
-            frame_obj (FrameObj): Frame object storing OpenCV frame and timestamp.
-            track_obj (BoundingBoxes): List of bounding boxes from tracking stage.
-                list has to be of the same length as the list of bounding boxes in track_obj, and ordered in the same
-                way (feature vector i belongs to box i).
-            re_id_data (ReidData): Data class containing data about tracked subjects.
-
-        Returns:
-            BoundingBoxes: object containing all re-id tracked boxes (bounding boxes where re-id is performed).
-        """
-
-        tracked_bounding_boxes = track_obj.bounding_boxes
-        box_features = self.extract_features_boxes(frame_obj, track_obj)
-
-        # Copy the original bounding boxes to a new list.
-        bounding_boxes = copy.copy(tracked_bounding_boxes)
-
-        # Loop over all objects being followed.
-        for query_id in re_id_data.get_queries():
-            query_feature = re_id_data.get_feature_for_query(query_id)
-
-            # List 'track_features' contains feature vectors in same order as bounding boxes.
-            # Loop over the detected features in the frame.
-            for i, feature in enumerate(box_features):
-                # If the bounding box is already assigned to an object, don't compare it.
-                if tracked_bounding_boxes[i].object_id is None:
-                    # Calculate the similarity value of the 2 feature vectors.
-                    similarity_value = self.similarity(query_feature, feature)
-                    if similarity_value > self.threshold:
-                        box_id = tracked_bounding_boxes[i].identifier
-
-                        # Store that this box id belongs to a certain object id.
-                        re_id_data.add_query_box(box_id, query_id)
-
-                        # Update object id of the box.
-                        bounding_boxes[i] = BoundingBox(
-                            identifier=box_id,
-                            rectangle=tracked_bounding_boxes[i].rectangle,
-                            classification=tracked_bounding_boxes[i].classification,
-                            certainty=tracked_bounding_boxes[i].certainty,
-                            object_id=query_id
-                        )
-
-                        print(f"Re-Id of object {query_id} in box {box_id}")
-
-        return BoundingBoxes(bounding_boxes)
